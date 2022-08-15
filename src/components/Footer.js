efb31572665c3599dfdd7f66a8a1145bc9599b80
Joeself <43977121+J-VU@users.noreply.github.com>
2022-08-15 19:09:13 -0400

    chore/style: requested text change (#153)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ const Footer = () => {
   return (
     <>
       <div className='footer desktop-version'>
-        <p className='company'>nao now LLC</p>
+        <p className='company'>Nao Now LLC</p>
         <p>
           Made with <img src={HeartImg} alt='' /> in sunny California.
         </p>
         <p>Copyright 2021; All rights reserved.</p>
       </div>
       <div className='footer mobile-version'>
-        <p className='company'>nao now LLC</p>
+        <p className='company'>Nao Now LLC</p>
         <p>
           Made with <img src={HeartImg} alt='' /> in sunny California.
         </p>
