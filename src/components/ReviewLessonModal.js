83c7b0ff8b76c215f74cc9c334a4bc8e52a1e33b
Laroikin <said24abdi04@gmail.com>
2022-12-08 22:20:10 +0900

    feat: add get and validate data using react-hook-form

--- src/components/ReviewLessonModal.js
+++ src/components/ReviewLessonModal.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import Modal from 'react-modal'
-import { useState } from 'react'
 import Tutor from '../assets/images/tutor.png'
 import { ReactComponent as Close } from '../assets/images/Close icon.svg'
-import ImgStarOutline from '../assets/images/star_outline_red.svg'
-import ImgStarFill from '../assets/images/star_fill_red.svg'
+import { useForm, Controller } from 'react-hook-form'
+import ReviewRating from './ReviewRating'
 
 const ReviewLessonModal = ({ isOpen, setIsOpen, lessonInfo }) => {
+  const { register, handleSubmit, control, reset } = useForm()
   const closeModal = () => {
     setIsOpen(false)
+    reset()
   }
-  const [isChecked, onCheck] = useState()
-  const [starIndex, setStarIndex] = useState()
 
   lessonInfo = {
     subjectName: 'English',
@@ -31,6 +30,11 @@ const ReviewLessonModal = ({ isOpen, setIsOpen, lessonInfo }) => {
     hour: 'numeric'
   }).format(new Date(lessonInfo.date.getTime() + lessonInfo.duration * 60000))
 
+  const onSubmit = data => {
+    console.log(data)
+    closeModal()
+  }
+
   return (
     <>
       <Modal
@@ -38,6 +42,7 @@ const ReviewLessonModal = ({ isOpen, setIsOpen, lessonInfo }) => {
         onRequestClose={closeModal}
         overlayClassName='review-lesson-modal-overlay'
         className='review-lesson-modal'
+        bodyOpenClassName={'review-modal-open'}
       >
         <div className='header'>
           <h3>Lesson Feedback</h3>
@@ -54,7 +59,7 @@ const ReviewLessonModal = ({ isOpen, setIsOpen, lessonInfo }) => {
           </div>
           <img src={lessonInfo.tutorPic} alt={lessonInfo.tutorName} />
         </div>
-        <div className='body'>
+        <form onSubmit={handleSubmit(onSubmit)} className='body'>
           <h4>Why am I being asked this?</h4>
           <p>
             Providing feedback helps NaoNow create a better experience for
@@ -71,11 +76,18 @@ const ReviewLessonModal = ({ isOpen, setIsOpen, lessonInfo }) => {
                 name='attendance'
                 value='present'
                 defaultChecked
+                {...register('attendance')}
               />
               Tutor was present
             </label>
             <label htmlFor='late'>
-              <input id='late' type='radio' name='attendance' value='late' />
+              <input
+                id='late'
+                type='radio'
+                name='attendance'
+                value='late'
+                {...register('attendance')}
+              />
               Tutor was late
             </label>
             <label htmlFor='noshow'>
@@ -84,32 +96,30 @@ const ReviewLessonModal = ({ isOpen, setIsOpen, lessonInfo }) => {
                 type='radio'
                 name='attendance'
                 value='noshow'
+                {...register('attendance')}
               />
               No show
             </label>
           </div>
           <h4>Rate this tutor</h4>
-          <div className='rating'>
-            <div className='stars'>
-              {[0, 1, 2, 3, 4].map((value, index) => (
-                <img
-                  key={`img-${index}`}
-                  src={starIndex >= index ? ImgStarFill : ImgStarOutline}
-                  alt=''
-                  onClick={() => setStarIndex(index)}
-                />
-              ))}
-            </div>
-          </div>
+
+          <Controller
+            control={control}
+            name='rating'
+            render={({ field: { onChange } }) => (
+              <ReviewRating onChange={onChange} />
+            )}
+          />
 
           <h4>Write a review</h4>
           <textarea
             name='review'
             id='review'
             placeholder='Write your review here...'
+            {...register('review')}
           ></textarea>
           <button type='submit'>Submit Review</button>
-        </div>
+        </form>
       </Modal>
     </>
   )
