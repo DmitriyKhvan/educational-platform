e4a9f65158cfa410725fa74ed774301b8842d013
Laroikin <said24abdi04@gmail.com>
2022-12-15 08:44:34 +0900

    feat: finish modal without file upload

--- src/components/tutor-dashboard/ReviewLessonModal.js
+++ src/components/tutor-dashboard/ReviewLessonModal.js
@@ -4,6 +4,7 @@ import Tutor from '../../assets/images/tutor.png'
 import { ReactComponent as Close } from '../../assets/images/Close icon.svg'
 import { useForm, Controller } from 'react-hook-form'
 import ReviewRating from '../ReviewRating'
+import RadioGroup from '../RadioGroup'
 
 const ReviewLessonModal = ({ isOpen, setIsOpen, lessonInfo }) => {
   const { register, handleSubmit, control, reset } = useForm()
@@ -14,8 +15,9 @@ const ReviewLessonModal = ({ isOpen, setIsOpen, lessonInfo }) => {
 
   lessonInfo = {
     subjectName: 'English',
-    tutorPic: Tutor,
-    tutorName: 'Sarah',
+    studentPic: Tutor,
+    studentName: 'Sarah B.',
+    level: 2,
     duration: '60',
     date: new Date()
   }
@@ -40,85 +42,63 @@ const ReviewLessonModal = ({ isOpen, setIsOpen, lessonInfo }) => {
       <Modal
         isOpen={isOpen}
         onRequestClose={closeModal}
-        overlayClassName='review-lesson-modal-overlay'
-        className='review-lesson-modal'
-        bodyOpenClassName={'review-modal-open'}
+        overlayClassName='review-tutor-lesson-modal-overlay'
+        className='review-tutor-lesson-modal'
+        bodyOpenClassName={'review-tutor-modal-open'}
       >
-        <div className='header'>
-          <h3>Lesson Feedback</h3>
-          <button onClick={closeModal}>
-            <Close />
-          </button>
-        </div>
         <div className='subject-info'>
           <div className='left'>
-            <h1>{lessonInfo.subjectName}</h1>
-            <p>
-              {lessonStartDate} → {lessonEndDate}
-            </p>
+            <img src={lessonInfo.studentPic} alt={lessonInfo.studentName} />
+            <h1>{lessonInfo.studentName}</h1>
           </div>
-          <img src={lessonInfo.tutorPic} alt={lessonInfo.tutorName} />
+          <span>{'Level ' + lessonInfo.level}</span>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)} className='body'>
-          <h4>Why am I being asked this?</h4>
-          <p>
-            Providing feedback helps NaoNow create a better experience for
-            students and tutors.
-          </p>
-          <h4>Will tutor see my responses?</h4>
-          <p>No, your lesson review will be kept private.</p>
-          <h4>Tutor attendance</h4>
-          <div className='attendance-selector'>
-            <label htmlFor='present'>
-              <input
-                id='present'
-                type='radio'
-                name='attendance'
-                value='present'
-                defaultChecked
-                {...register('attendance')}
-              />
-              Tutor was present
-            </label>
-            <label htmlFor='late'>
-              <input
-                id='late'
-                type='radio'
-                name='attendance'
-                value='late'
-                {...register('attendance')}
-              />
-              Tutor was late
-            </label>
-            <label htmlFor='noshow'>
-              <input
-                id='noshow'
-                type='radio'
-                name='attendance'
-                value='noshow'
-                {...register('attendance')}
-              />
-              No show
-            </label>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='body'>
+            <h1>Nao Now Level 2: Golden Gate Bridge</h1>
+            <h4>Write a review</h4>
+            <textarea
+              name='review'
+              id='review'
+              placeholder='Write your review here...'
+              {...register('review')}
+            ></textarea>
           </div>
-          <h4>Rate this tutor</h4>
 
-          <Controller
-            control={control}
-            name='rating'
-            render={({ field: { onChange } }) => (
-              <ReviewRating onChange={onChange} />
-            )}
-          />
+          <div className='modal-footer'>
+            <div className='buttons'>
+              <button className='close' onClick={closeModal}>Close</button>
+              <button type='submit'>Submit Review</button>
+            </div>
 
-          <h4>Write a review</h4>
-          <textarea
-            name='review'
-            id='review'
-            placeholder='Write your review here...'
-            {...register('review')}
-          ></textarea>
-          <button type='submit'>Submit Review</button>
+            <div className='attendance'>
+              <h4>Student Attendance</h4>
+              <Controller
+                control={control}
+                name='attendance'
+                render={({ field: { onChange, name } }) => (
+                  <RadioGroup
+                    onChange={onChange}
+                    options={[
+                      {
+                        name: 'present',
+                        text: 'Student was present'
+                      },
+                      {
+                        name: 'late',
+                        text: 'Student was late'
+                      },
+                      {
+                        name: 'noshow',
+                        text: 'No show'
+                      }
+                    ]}
+                    name={name}
+                  />
+                )}
+              ></Controller>
+            </div>
+          </div>
         </form>
       </Modal>
     </>
