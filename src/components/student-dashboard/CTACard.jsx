c589e199f15220ee825310e49abb83acc3f6b5be
almazesh <eshimbekovalmaz.ib.1.19@gmail.com>
2023-03-30 08:20:36 +0600

    Fix bugs on figma

--- src/components/student-dashboard/CTACard.jsx
+++ src/components/student-dashboard/CTACard.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
 
 const CTACard = ({ icon, title, subtitle, button, color }) => (
-  <div className={`page-card ${color} small-card p-3 mx-3 ms-3`}>
-    <div className='pt-3 pb-3 container'>
-      <img src={icon} alt='' className='img-fluid w-15 mb-2' />
-      <h3 className='text-white'>{title}</h3>
+  <div style={{background: color}} className={`page-card ${title} small-card  mx-4 ms-3`}>
+    <div className='p-0 container'>
+      <section className='small-card-inline'>
+        <div className='small-card-img-rounded'>
+          <img src={icon} alt='' className='img-fluid ' />
+        </div>
+        <h3 className='text-white'>{title}</h3>
+      </section>
       {
         button
           ? <Link
