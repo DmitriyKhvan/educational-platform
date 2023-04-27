60f9d880431771a35bdfedac368f734d71b7861d
lxhan <lxhan@naver.com>
2023-04-27 20:17:54 +0900

    conflicts

--- src/pages/Tutors/Availiability/AvailabilityPicker.jsx
+++ src/pages/Tutors/Availiability/AvailabilityPicker.jsx
@@ -1,216 +1,216 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import { useSelector, useDispatch } from 'react-redux'
-import moment from 'moment'
-import { AvailProv } from './AvailabilityProvider'
-import trashCan from '../../../assets/images/trash_can.svg'
-import Alert from '../../../components/Popup/Alert'
-import Select from 'react-select'
-import { useForm } from 'react-hook-form'
+import React, { useContext, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector, useDispatch } from 'react-redux';
+import moment from 'moment';
+import { AvailProv } from './AvailabilityProvider';
+import trashCan from '../../../assets/images/trash_can.svg';
+import Alert from '../../../components/Popup/Alert';
+import Select from 'react-select';
+import { useForm } from 'react-hook-form';
 import findIndex from 'lodash/findIndex';
 
-const formatTime = time => {
-  return moment.utc(time * 1000).format('HH:mm')
-}
+const formatTime = (time) => {
+  return moment.utc(time * 1000).format('HH:mm');
+};
 
-const formatTimeToSeconds = time => {
+const formatTimeToSeconds = (time) => {
   const [hours, minutes] = time.split(':');
-  return ((parseInt(hours) * 60 * 60) + (parseInt(minutes) * 60));
-}
+  return parseInt(hours) * 60 * 60 + parseInt(minutes) * 60;
+};
 
 const times = [
   {
-    timeType: "12:00 AM",
-    time: 0
+    timeType: '12:00 AM',
+    time: 0,
   },
   {
-    timeType: "12:30 AM",
-    time: 1800
+    timeType: '12:30 AM',
+    time: 1800,
   },
 
   {
-    timeType: "01:00 AM",
-    time: 3600
+    timeType: '01:00 AM',
+    time: 3600,
   },
   {
-    timeType: "01:30 AM",
-    time: 5400
+    timeType: '01:30 AM',
+    time: 5400,
   },
   {
-    timeType: "02:00 AM",
-    time: 7200
+    timeType: '02:00 AM',
+    time: 7200,
   },
   {
-    timeType: "02:30 AM",
-    time: 9000
+    timeType: '02:30 AM',
+    time: 9000,
   },
   {
-    timeType: "03:00 AM",
-    time: 10800
+    timeType: '03:00 AM',
+    time: 10800,
   },
   {
-    timeType: "03:30 AM",
-    time: 12600
+    timeType: '03:30 AM',
+    time: 12600,
   },
   {
-    timeType: "04:00 AM",
-    time: 14400
+    timeType: '04:00 AM',
+    time: 14400,
   },
   {
-    timeType: "04:30 AM",
-    time: 16200
+    timeType: '04:30 AM',
+    time: 16200,
   },
   {
-    timeType: "05:00 AM",
-    time: 18000
+    timeType: '05:00 AM',
+    time: 18000,
   },
   {
-    timeType: "05:30 AM",
-    time: 19800
+    timeType: '05:30 AM',
+    time: 19800,
   },
   {
-    timeType: "06:00 AM",
-    time: 21600
+    timeType: '06:00 AM',
+    time: 21600,
   },
   {
-    timeType: "06:30 AM",
-    time: 23400
+    timeType: '06:30 AM',
+    time: 23400,
   },
   {
-    timeType: "07:00 AM",
-    time: 25200
+    timeType: '07:00 AM',
+    time: 25200,
   },
   {
-    timeType: "07:30 AM",
-    time: 27000
+    timeType: '07:30 AM',
+    time: 27000,
   },
   {
-    timeType: "08:00 AM",
-    time: 28800
+    timeType: '08:00 AM',
+    time: 28800,
   },
   {
-    timeType: "08:30 AM",
-    time: 30600
+    timeType: '08:30 AM',
+    time: 30600,
   },
   {
-    timeType: "09:00 AM",
-    time: 32400
+    timeType: '09:00 AM',
+    time: 32400,
   },
   {
-    timeType: "09:30 AM",
-    time: 34200
+    timeType: '09:30 AM',
+    time: 34200,
   },
   {
-    timeType: "10:00 AM",
-    time: 36000
+    timeType: '10:00 AM',
+    time: 36000,
   },
   {
-    timeType: "10:30 AM",
-    time: 37800
+    timeType: '10:30 AM',
+    time: 37800,
   },
   {
-    timeType: "11:00 AM",
-    time: 39600
+    timeType: '11:00 AM',
+    time: 39600,
   },
   {
-    timeType: "11:30 AM",
-    time: 41400
+    timeType: '11:30 AM',
+    time: 41400,
   },
   {
-    timeType: "12:00 PM",
-    time: 43200
+    timeType: '12:00 PM',
+    time: 43200,
   },
   {
-    timeType: "12:30 PM",
-    time: 45000
+    timeType: '12:30 PM',
+    time: 45000,
   },
   {
-    timeType: "01:00 PM",
-    time: 46800
+    timeType: '01:00 PM',
+    time: 46800,
   },
   {
-    timeType: "01:30 PM",
-    time: 48600
+    timeType: '01:30 PM',
+    time: 48600,
   },
   {
-    timeType: "02:00 PM",
-    time: 50400
+    timeType: '02:00 PM',
+    time: 50400,
   },
   {
-    timeType: "02:30 PM",
-    time: 52200
+    timeType: '02:30 PM',
+    time: 52200,
   },
   {
-    timeType: "03:00 PM",
-    time: 54000
+    timeType: '03:00 PM',
+    time: 54000,
   },
   {
-    timeType: "03:30 PM",
-    time: 55800
+    timeType: '03:30 PM',
+    time: 55800,
   },
   {
-    timeType: "04:00 PM",
-    time: 57600
+    timeType: '04:00 PM',
+    time: 57600,
   },
   {
-    timeType: "04:30 PM",
-    time: 59400
+    timeType: '04:30 PM',
+    time: 59400,
   },
   {
-    timeType: "05:00 PM",
-    time: 61200
+    timeType: '05:00 PM',
+    time: 61200,
   },
   {
-    timeType: "05:30 PM",
-    time: 63000
+    timeType: '05:30 PM',
+    time: 63000,
   },
   {
-    timeType: "06:00 PM",
-    time: 64800
+    timeType: '06:00 PM',
+    time: 64800,
   },
   {
-    timeType: "06:30 PM",
-    time: 66600
+    timeType: '06:30 PM',
+    time: 66600,
   },
   {
-    timeType: "07:00 PM",
-    time: 68400
+    timeType: '07:00 PM',
+    time: 68400,
   },
   {
-    timeType: "07:30 PM",
-    time: 70200
+    timeType: '07:30 PM',
+    time: 70200,
   },
   {
-    timeType: "08:00 PM",
-    time: 72000
+    timeType: '08:00 PM',
+    time: 72000,
   },
   {
-    timeType: "08:30 PM",
-    time: 73800
+    timeType: '08:30 PM',
+    time: 73800,
   },
   {
-    timeType: "09:00 PM",
-    time: 75600
+    timeType: '09:00 PM',
+    time: 75600,
   },
   {
-    timeType: "09:30 PM",
-    time: 77400
+    timeType: '09:30 PM',
+    time: 77400,
   },
   {
-    timeType: "10:00 PM",
-    time: 79200
+    timeType: '10:00 PM',
+    time: 79200,
   },
   {
-    timeType: "10:30 PM",
-    time: 81000
+    timeType: '10:30 PM',
+    time: 81000,
   },
   {
-    timeType: "11:00 PM",
-    time: 82800
+    timeType: '11:00 PM',
+    time: 82800,
   },
   {
-    timeType: "11:30 PM",
-    time: 84600
+    timeType: '11:30 PM',
+    time: 84600,
   },
 ];
 
@@ -249,9 +249,7 @@ const AvailabilityPicker = ({
   const [toTime, setToTime] = useState(tTime);
   const [currentData, setCurrentData] = useState([]);
 
-  const {
-    control
-  } = useForm()
+  const { control } = useForm();
 
   useEffect(() => {
     setCurrentData(tutorInfo.availabilities[day]);
@@ -288,7 +286,7 @@ const AvailabilityPicker = ({
         setCurrentData(cpyCurrentData || []);
       }
     }
-  }
+  };
 
   const removeRowDown = (type) => {
     Alert(
@@ -311,51 +309,53 @@ const AvailabilityPicker = ({
     removeAvailabilityRow(item);
   };
 
-  const fromTimeIndex = findIndex(timeOptions, { value: formatTimeToSeconds(fromTime) });
-  const toTimeIndex = findIndex(timeOptions, { value: formatTimeToSeconds(toTime) });
+  const fromTimeIndex = findIndex(timeOptions, {
+    value: formatTimeToSeconds(fromTime),
+  });
+  const toTimeIndex = findIndex(timeOptions, {
+    value: formatTimeToSeconds(toTime),
+  });
 
   return (
-    <div className='row mx-0 mt-2'>
-      <div className='col-auto align_time_img-time over_form'>
-        <div className='d-flex '>
+    <div className="row mx-0 mt-2">
+      <div className="col-auto align_time_img-time over_form">
+        <div className="d-flex ">
           <Select
-            className='time_picker text-center ' 
+            className="time_picker text-center "
             defaultValue={timeOptions[fromTimeIndex]}
             options={timeOptions}
-            onChange={e => {
-              onChangeTime(e.value, 'newTime', 'from')
+            onChange={(e) => {
+              onChangeTime(e.value, 'newTime', 'from');
             }}
           />
         </div>
       </div>
 
-      <div className='col-auto align-self-center pickerToText '>
-        <span className='text-muted availability_to_text over_to_text'>
-          TO
-        </span>
+      <div className="col-auto align-self-center pickerToText ">
+        <span className="text-muted availability_to_text over_to_text">TO</span>
       </div>
-      <div className='col-auto align_time_img-time over_to'>
-        <div className='d-flex '>
-            <Select
-            className='time_picker text-center' 
+      <div className="col-auto align_time_img-time over_to">
+        <div className="d-flex ">
+          <Select
+            className="time_picker text-center"
             defaultValue={timeOptions[toTimeIndex]}
             options={timeOptions}
-            onChange={e => {
-              onChangeTime(e.value, 'newTime', 'to')
+            onChange={(e) => {
+              onChangeTime(e.value, 'newTime', 'to');
             }}
           />
         </div>
       </div>
-      <div className='col-auto align-self-center '>
+      <div className="col-auto align-self-center ">
         <button
-          type='button'
-          className='btn fa_trash_can ms-3 pb-0 align_delete'
+          type="button"
+          className="btn fa_trash_can ms-3 pb-0 align_delete"
           onClick={() => removeRowDown(type)}
         >
-          <img src={trashCan} className='fa_icon' alt='' />
+          <img src={trashCan} className="fa_icon" alt="" />
         </button>
       </div>
     </div>
-  )
-}
-export default AvailabilityPicker
+  );
+};
+export default AvailabilityPicker;
