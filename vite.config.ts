d7a1876b40ee6a1f5848e9b88fb97e31fa980ba2
Laroikin <said24abdi04@gmail.com>
2023-05-31 14:02:19 +0900

    chore: add analyze script

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,35 @@
-import { defineConfig } from 'vite';
+import { build, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import envCompatible from 'vite-plugin-env-compatible';
-const ENV_PREFIX = "REACT_APP_";
+import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
+import { splitVendorChunkPlugin } from 'vite';
+const ENV_PREFIX = 'REACT_APP_';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), envCompatible({ prefix: ENV_PREFIX })],
-  mode: "development",
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    envCompatible({ prefix: ENV_PREFIX }),
+    viteCompression(),
+    splitVendorChunkPlugin(),
+    {
+      ...visualizer({
+        template: 'treemap',
+        gzipSize: true,
+        brotliSize: true,
+        open: true,
+      }),
+      apply(config, { mode, command }) {
+        return command === 'build' && mode === 'analyze' ? true : false;
+      },
+    },
+  ],
+  build: {
+    reportCompressedSize: true,
+  },
 });
