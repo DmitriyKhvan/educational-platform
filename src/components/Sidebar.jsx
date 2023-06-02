5f95975a87eef47dc935950786baee0c1d37c68f
Laroikin <said24abdi04@gmail.com>
2023-06-02 19:31:36 +0900

    fix: fix mobile sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,6 @@ import { useAuth } from '../modules/auth';
 import CloseIcon from '../assets/images/close.svg';
 import Logo from '../assets/images/logo.png';
 import LogoutImg from '../assets/images/logout_icon.svg';
-import referIcon from '../assets/images/referIconActive.png';
 import referActiveIcon from '../assets/images/referIconActive.png';
 import whiteCalendar from '../assets/images/sidebar/active-calendar.png';
 import Icon16 from '../assets/images/sidebar/icon16.svg';
@@ -17,39 +16,15 @@ import tutorIcon from '../assets/images/sidebar/purple_tutor_icon.svg';
 import tutorActiveIcon from '../assets/images/sidebar/white_tutor_icon.svg';
 import Icon2 from '../assets/images/sidebar/purple_lesson_icon.svg';
 import Icon11 from '../assets/images/sidebar/purple_subscription_icon.svg';
-import MessageIcon from '../assets/images/sidebar/purple_message_icon.svg';
-import MessageActiveIcon from '../assets/images/sidebar/white_message_icon.svg';
 import purpleCalendar from '../assets/images/sidebar/calendar.png';
-import GameIcon from '../assets/images/sidebar/game-menuitem.svg';
-import GameIconFilled from '../assets/images/sidebar/game-menuitem-filled.svg';
 import ClassMaterialIcon from '../assets/images/sidebar/class_materials.svg';
-// import purplePayment from '../assets/images/sidebar/payment.png'
-// import whitePayment from '../assets/images/sidebar/active-payment.png'
-// import purpleStudentList from '../assets/images/sidebar/student-list.png'
-// import whiteStudentList from '../assets/images/sidebar/active-student-list.png'
-
 import ActiveIcon1 from '../assets/images/sidebar/white_dashboard_icon.svg';
 import ActiveIcon2 from '../assets/images/sidebar/white_lesson_icon.svg';
-// import ActiveIcon4 from '../assets/images/sidebar/active4.svg'
-// import ActiveIcon5 from '../assets/images/sidebar/white_message_icon.svg'
-// import ActiveIcon9 from '../assets/images/sidebar/white_tutor_icon.svg'
 import ActiveIcon11 from '../assets/images/sidebar/white_subscription_icon.svg';
-// import ActiveIcon12 from '../assets/images/sidebar/active12.svg'
 import ActiveIcon16 from '../assets/images/sidebar/active16.svg';
 import ActiveIcon17 from '../assets/images/sidebar/active17.svg';
 import ActiveIcon18 from '../assets/images/sidebar/active18.svg';
-// import ActiveIcon19 from '../assets/images/sidebar/active19.svg'
-// import whiteClassMaterialIcon from '../assets/images/sidebar/white_class_material_icon.svg'
-// import purpleClassMaterialIcon from '../assets/images/sidebar/purple_class_material_icon.svg'
-// import whiteHomeworklIcon from '../assets/images/sidebar/white_homework_icon.svg'
-// import purpleHomeworklIcon from '../assets/images/sidebar/purple_homework_icon.svg'
-// import whiteSupportlIcon from '../assets/images/sidebar/white_support_icon.svg'
-// import purpleSupportlIcon from '../assets/images/sidebar/purple_support_icon.svg'
-// import whiteFeedbacklIcon from '../assets/images/sidebar/white_feedback_icon.svg'
-// import purpleFeedbacklIcon from '../assets/images/sidebar/purple_feedback_icon.svg'
-// import whiteRewardlIcon from '../assets/images/sidebar/white_reward_icon.svg'
-// import purpleRewardlIcon from '../assets/images/sidebar/purple_reward_icon.svg'
-import { classMaterialURL, gameLinkURL } from '../constants/global';
+import { classMaterialURL } from '../constants/global';
 
 const tutorNavLinks = [
   {
@@ -70,12 +45,6 @@ const tutorNavLinks = [
     icon: tutorIcon,
     activeIcon: tutorActiveIcon,
   },
-  // {
-  //   label: "Messages",
-  //   link:"/messages/",
-  //   icon:MessageIcon,
-  //   activeIcon:MessageActiveIcon
-  // },
   {
     label: 'check_requests',
     link: '/approve-requests',
@@ -88,27 +57,6 @@ const tutorNavLinks = [
     icon: purpleCalendar,
     activeIcon: whiteCalendar,
   },
-  // {
-  //   label: 'student_list',
-  //   link: '/tutor/students',
-  //   icon: purpleStudentList,
-  //   activeIcon: whiteStudentList
-  // },
-  // {
-  //   label: 'messages',
-  //   link: '/messages',
-  //   icon: Icon5,
-  //   activeIcon: ActiveIcon5
-  // },
-  // { label: 'class_materials', link: '/tutor/class-materials', icon: Icon10, activeIcon: Icon10 },
-  // {
-  //   label: 'payment',
-  //   link: '/tutor/payment-page',
-  //   icon: purplePayment,
-  //   activeIcon: whitePayment
-  // }
-  // { divider: true },
-  // { label: 'feedback', link: '/feedback', icon: Icon14, activeIcon: ActiveIcon14 }
 ];
 
 const studentNavLinks = [
@@ -124,12 +72,6 @@ const studentNavLinks = [
     icon: Icon2,
     activeIcon: ActiveIcon2,
   },
-  // {
-  //   label: "Messages",
-  //   link:"/messages/",
-  //   icon:MessageIcon,
-  //   activeIcon:MessageActiveIcon
-  // },
   {
     label: 'mentors',
     link: '/student/mentors-list',
@@ -142,13 +84,6 @@ const studentNavLinks = [
     icon: Icon1,
     activeIcon: referActiveIcon,
   },
-  // {
-  //   label: 'games',
-  //   link: gameLinkURL,
-  //   icon: GameIcon,
-  //   activeIcon: GameIconFilled,
-  //   external: true
-  // },
   {
     label: 'class_material',
     link: classMaterialURL || 'https://naonow.instructure.com/',
@@ -156,49 +91,6 @@ const studentNavLinks = [
     activeIcon: ClassMaterialIcon,
     external: true,
   },
-  // {
-  //   label: 'messages',
-  //   link: '/messages',
-  //   icon: Icon5,
-  //   activeIcon: ActiveIcon5
-  // },
-  // {
-  //   label: 'favorite_tutors',
-  //   link: '/student/favorite-tutors',
-  //   icon: Icon9,
-  //   activeIcon: ActiveIcon9
-  // },
-  // {
-  //   label: 'sidebar_homework_title',
-  //   link: '/',
-  //   icon: purpleHomeworklIcon,
-  //   activeIcon: whiteHomeworklIcon
-  // },
-  // {
-  //   label: 'packages',
-  //   link: '/student/packages',
-  //   icon: Icon11,
-  //   activeIcon: ActiveIcon11
-  // },
-  { divider: true },
-  // {
-  //   label: 'support',
-  //   link: '/support',
-  //   icon: purpleSupportlIcon,
-  //   activeIcon: whiteSupportlIcon
-  // },
-  // {
-  //   label: 'feedback',
-  //   link: '/feedback',
-  //   icon: purpleFeedbacklIcon,
-  //   activeIcon: whiteFeedbacklIcon
-  // },
-  // {
-  //   label: 'faq',
-  //   link: '/faq',
-  //   icon: purpleRewardlIcon,
-  //   activeIcon: whiteRewardlIcon
-  // }
 ];
 
 const adminNavLinks = [
@@ -275,6 +167,10 @@ const Sidebar = () => {
     dispatch({ type: 'HIDE_SIDEBAR' });
   };
 
+  console.log(navLinks);
+
+  console.log(t(navLinks?.[1]?.label, { ns: 'sidebar' }));
+
   return (
     <>
       <div className="side-bar desktop-version">
@@ -285,57 +181,56 @@ const Sidebar = () => {
           alt=""
         />
         <div className="link-list">
-          {navLinks &&
-            navLinks.map((item, index) =>
-              item.divider ? (
-                <div key={`divider-${index}`} className="divider" />
-              ) : (
-                <li
-                  className={`nav-item ${item.is_selected ? 'active' : ''}`}
-                  key={index}
-                >
-                  {item.external ? (
-                    <a
-                      href="#"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        window.open(item.link);
-                      }}
-                    >
-                      <div className="icon">
-                        <img
-                          src={item.activeIcon}
-                          alt=""
-                          className={item.is_selected ? 'block' : 'none'}
-                        />
-                        <img
-                          src={item.icon}
-                          alt=""
-                          className={!item.is_selected ? 'block' : 'none'}
-                        />
-                      </div>
-                      <span>{t(item.label, { ns: 'sidebar' })}</span>
-                    </a>
-                  ) : (
-                    <Link to={item.link}>
-                      <div className="icon">
-                        <img
-                          src={item.activeIcon}
-                          alt=""
-                          className={item.is_selected ? 'block' : 'none'}
-                        />
-                        <img
-                          src={item.icon}
-                          alt=""
-                          className={!item.is_selected ? 'block' : 'none'}
-                        />
-                      </div>
-                      <span>{t(item.label, { ns: 'sidebar' })}</span>
-                    </Link>
-                  )}
-                </li>
-              ),
-            )}
+          {navLinks?.map((item, index) =>
+            item.divider ? (
+              <div key={`divider-${index}`} className="divider" />
+            ) : (
+              <li
+                className={`nav-item ${item.is_selected ? 'active' : ''}`}
+                key={index}
+              >
+                {item.external ? (
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      window.open(item.link);
+                    }}
+                  >
+                    <div className="icon">
+                      <img
+                        src={item.activeIcon}
+                        alt=""
+                        className={item.is_selected ? 'block' : 'none'}
+                      />
+                      <img
+                        src={item.icon}
+                        alt=""
+                        className={!item.is_selected ? 'block' : 'none'}
+                      />
+                    </div>
+                    <span>{t(item.label, { ns: 'sidebar' })}</span>
+                  </a>
+                ) : (
+                  <Link to={item.link}>
+                    <div className="icon">
+                      <img
+                        src={item.activeIcon}
+                        alt=""
+                        className={item.is_selected ? 'block' : 'none'}
+                      />
+                      <img
+                        src={item.icon}
+                        alt=""
+                        className={!item.is_selected ? 'block' : 'none'}
+                      />
+                    </div>
+                    <span>{t(item.label, { ns: 'sidebar' })}</span>
+                  </Link>
+                )}
+              </li>
+            ),
+          )}
         </div>
       </div>
       {isShowSidebar ? (
@@ -349,29 +244,28 @@ const Sidebar = () => {
           />
           <div className="divider" />
           <div className="link-list">
-            {navLinks &&
-              navLinks.map((item, index) => (
-                <li
-                  className={`nav-item ${item.is_selected ? 'active' : ''}`}
-                  key={index}
-                >
-                  <Link to={item.link} onClick={() => hideSidebar()}>
-                    <div className="icon">
-                      <img
-                        src={item.activeIcon}
-                        alt=""
-                        className={item.is_selected ? 'block' : 'none'}
-                      />
-                      <img
-                        src={item.icon}
-                        alt=""
-                        className={!item.is_selected ? 'block' : 'none'}
-                      />
-                    </div>
-                    <span>{item.label}</span>
-                  </Link>
-                </li>
-              ))}
+            {navLinks?.map((item, index) => (
+              <li
+                className={`nav-item ${item.is_selected ? 'active' : ''}`}
+                key={index}
+              >
+                <Link to={item.link} onClick={() => hideSidebar()}>
+                  <div className="icon">
+                    <img
+                      src={item.activeIcon}
+                      alt=""
+                      className={item.is_selected ? 'block' : 'none'}
+                    />
+                    <img
+                      src={item.icon}
+                      alt=""
+                      className={!item.is_selected ? 'block' : 'none'}
+                    />
+                  </div>
+                  <span>{t(item.label, { ns: 'sidebar' })}</span>
+                </Link>
+              </li>
+            ))}
             <li className="nav-item log-out" onClick={() => handleLogout()}>
               <div className="icon">
                 <img src={LogoutImg} alt="" />
