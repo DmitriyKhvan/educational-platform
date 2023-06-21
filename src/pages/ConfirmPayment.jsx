ff98f556145c08a7b2d78340ff7998a29ec8b046
Laroikin <said24abdi04@gmail.com>
2023-06-21 21:20:43 +0900

    feat: finishing payment flow

--- src/pages/ConfirmPayment.jsx
+++ src/pages/ConfirmPayment.jsx
@@ -1,5 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import Loader from '../components/Loader/Loader';
+import { faCheckCircle, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 export default function ConfirmPayment() {
-  return <div>Payment Confirmed</div>;
+  const clientSecret = new URLSearchParams(window.location.search).get(
+    'payment_intent_client_secret',
+  );
+
+  const [message, setMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const stripe = stripePromise;
+    stripe.then((res) => {
+      res.confirmCardPayment(clientSecret).then((result) => {
+        if (result.error) {
+          setMessage(result.error.message);
+          setError(true);
+        } else {
+          setMessage('Your payment was successfull!');
+        }
+      });
+    });
+  }, [clientSecret]);
+
+  if (!message) return <Loader />;
+
+  if (error)
+    return (
+      <main className="flex items-center justify-center h-screen">
+        <div className="space-y-8 flex flex-col items-center">
+          <FontAwesomeIcon
+            icon={faXmarkCircle}
+            className="w-16 h-16 text-red-500"
+          />
+          <h1 className="font-bold">{message}</h1>
+          <a
+            href="/dashboard"
+            className="text-white bg-purple-500 px-4 py-2 rounded font-bold"
+          >
+            Go to Dashboard
+          </a>
+        </div>
+      </main>
+    );
+
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <div className="space-y-8 flex flex-col items-center">
+        <FontAwesomeIcon
+          icon={faCheckCircle}
+          className="w-16 h-16 text-green-500"
+        />
+        <h1 className="font-bold">{message}</h1>
+        <a
+          href="/dashboard"
+          className="text-white bg-purple-500 px-4 py-2 rounded font-bold"
+        >
+          Go to Dashboard
+        </a>
+      </div>
+    </main>
+  );
 }
--- src/pages/Students/BuyPackage.jsx
+++ src/pages/Students/BuyPackage.jsx
@@ -4,79 +4,55 @@ import Loader from '../../components/Loader/Loader';
 // eslint-disable-next-line import/no-unresolved
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { useHistory, useParams } from 'react-router-dom';
+import { useQuery, gql, useMutation } from '@apollo/client';
+import { toast } from 'react-toastify';
 
-const getPricing = async () => {
-  const data = new Promise((resolve) =>
-    setTimeout(
-      () =>
-        resolve({
-          id: 2,
-          name: 'Private English',
-          coverImage: 'https://dev.naonow.com/cad124f3c9d1255e33ca.png',
-          description:
-            'Learn the foundations of the English language. Speaking, Reading, and Writing skills developed with your mentor while learning about meaningful cultural topics designed to help students immerse themselves and “think” in English.',
-          types: [
-            {
-              id: 1,
-              name: '1 Lesson per week',
-              lessons_per_week: 1,
-              weeks: 4,
-              price: 50000,
-              length: 25,
-            },
-            {
-              id: 2,
-              name: '2 Lessons per week',
-              lessons_per_week: 2,
-              weeks: 4,
-              price: 90000,
-              length: 25,
-            },
-            {
-              id: 4,
-              name: '2 Lessons per week',
-              lessons_per_week: 2,
-              weeks: 4,
-              price: 90000,
-              length: 25,
-            },
-            {
-              id: 5,
-              name: '2 Lessons per week',
-              lessons_per_week: 2,
-              weeks: 4,
-              price: 90000,
-              length: 25,
-            },
-            {
-              id: 6,
-              name: '2 Lessons per week',
-              lessons_per_week: 2,
-              weeks: 4,
-              price: 90000,
-              length: 25,
-            },
-            {
-              id: 3,
-              name: '3 Lessons per week',
-              lessons_per_week: 3,
-              weeks: 4,
-              price: 120000,
-              length: 50,
-            },
-          ],
-        }),
-      500,
-    ),
-  );
-  return data;
-};
+const GET_COURSES = gql`
+  query GetCourse($id: Int!) {
+    course(id: $id) {
+      id
+      title
+      description
+      packages {
+        period
+        discount
+        id
+        totalSessions
+        sessionsPerWeek
+        sessionTime
+        price
+      }
+    }
+  }
+`;
+
+const CREATE_PAYMENT_INTENT = gql`
+  mutation CreatePaymentIntent($id: Int!) {
+    createPaymentIntent(packageId: $id) {
+      clientSecret
+    }
+  }
+`;
 
 export default function BuyPackage() {
-  const [lessonPackage, setLessonPackage] = useState(null);
+  const { packageId: courseId } = useParams();
+
+  const { error, data } = useQuery(GET_COURSES, {
+    variables: {
+      id: parseInt(courseId),
+    },
+  });
+
+  const [getSecret] = useMutation(CREATE_PAYMENT_INTENT, {
+    variables: {
+      id: parseInt(courseId),
+    },
+  });
+
+  const [courseData, setCourseData] = useState(null);
   const [selectedLength, setSelectedLength] = useState(null);
   const [uniqueLength, setUniqueLength] = useState([]);
-  const [selectedType, setSelectedType] = useState(null);
+  const [selectedPackage, setSelectedPackage] = useState(null);
 
   const [parent] = useAutoAnimate({
     duration: 450,
@@ -84,144 +60,189 @@ export default function BuyPackage() {
   });
 
   const history = useHistory();
-  const { packageId } = useParams();
 
   useEffect(() => {
-    getPricing().then((data) => {
-      setLessonPackage(data);
-      setSelectedLength(data?.types[0]?.length);
-      setUniqueLength([
-        ...new Set(data?.types?.map((item) => item.length) ?? []),
-      ]);
-    });
-  }, []);
+    if (!data?.course) return;
+    setCourseData(data.course);
+    setUniqueLength([
+      ...new Set(data.course?.packages?.map((item) => item.sessionTime) ?? []),
+    ]);
+    setSelectedLength(data?.course?.packages[0]?.sessionTime);
+  }, [data]);
 
   const handleSubmit = async (e) => {
-    if (selectedType) {
+    if (selectedPackage) {
       e.preventDefault();
-      const response = await fetch(`http://localhost:3000/secret`);
-      const data = await response.json();
-
-      if (response?.ok) {
-        history.replace(`${packageId}/payment/${data.client_secret}`);
+      const response = await getSecret({
+        variables: {
+          id: parseInt(selectedPackage.id),
+        },
+      });
+      if (response?.errors) {
+        toast.error(response.errors[0].message);
+      } else if (response?.data) {
+        const { clientSecret } = response.data.createPaymentIntent;
+        history.replace(`/purchase/${courseId}/payment/${clientSecret}`);
       }
     }
   };
 
-  if (!lessonPackage) return <Loader />;
+  if (!courseData) return <Loader />;
+
+  if (error) {
+    return <div>Something went wrong</div>;
+  }
 
   return (
-    <main className="bg-[url(https://dev.naonow.com/img/20210605-NAONOW-FINAL-2%202.png)] bg-cover min-h-screen flex flex-col justify-center items-center p-4 transition-all duration-300">
-      <div
-        className="bg-gray-200/90 backdrop-blur-md backdrop-saturate-200 border-gray-100/40 border p-4 rounded-xl max-w-5xl w-full cool-shadow flex flex-col gap-8 md:gap-6 md:flex-row bg-center transition-transform duration-300"
-        ref={parent}
-      >
+    <main className="bg-[url(https://dev.naonow.com/img/20210605-NAONOW-FINAL-2%202.png)] bg-cover h-screen overflow-auto">
+      <div className="flex flex-col justify-center items-center p-4 transition-all duration-300">
         <div
-          className={`rounded-lg bg-cover aspect-square w-full h-full p-3 flex flex-col justify-between`}
-          style={{
-            background: `linear-gradient(rgba(0,0,0,0.35),rgba(0,0,0,0.35)),url(${lessonPackage?.coverImage})`,
-          }}
+          className="bg-gray-200/90 backdrop-blur-md backdrop-saturate-200 border-gray-100/40 border p-4 rounded-xl max-w-5xl w-full cool-shadow flex flex-col gap-8 md:gap-6 md:flex-row bg-center transition-transform duration-300"
+          ref={parent}
         >
-          <h1 className="text-3xl font-bold text-transparent bg-clip-text drop-shadow-2xl bg-white">
-            {lessonPackage.name}
-          </h1>
-          <p className="text-gray-300 bg-gray-900/80 rounded-md p-2 text-sm max-w-4xl mt-auto">
-            {lessonPackage.description}
-          </p>
-        </div>
-        <hr className="border-gray-400/50 rounded-full border md:hidden" />
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-          className="w-full flex flex-col gap-3"
-        >
-          <p className="text-lg font-bold text-gray-700/90">
-            Choose the duration:
-          </p>
-          <div className="flex gap-2">
-            {uniqueLength?.map((length) => (
-              <div key={length}>
-                <input
-                  type="radio"
-                  id={length}
-                  name="duration"
-                  value={length}
-                  defaultChecked={length === selectedLength}
-                  className="hidden peer"
-                  onChange={(e) => {
-                    setSelectedLength(parseInt(e.currentTarget.value));
-                  }}
-                />
-                <label
-                  className="flex flex-row justify-between items-center bg-gray-100/80 backdrop-blur-md text-gray-700/90 backdrop-saturate-200 border-gray-100/40 border-2 py-2 px-4 rounded-xl hover:border-purple-300 duration-300 peer-checked:border-purple-600 hover:shadow-gray-900/5 hover:shadow-xl"
-                  htmlFor={length}
-                >
-                  <div className="flex flex-col">
-                    <p className="text-lg font-bold">{length} minutes</p>
-                  </div>
-                </label>
-              </div>
-            ))}
+          <div
+            className={`rounded-lg bg-cover aspect-square w-full h-full p-3 flex flex-col justify-between`}
+            style={{
+              background: `linear-gradient(rgba(0,0,0,0.35),rgba(0,0,0,0.35)),url(${courseData?.coverImage})`,
+            }}
+          >
+            <h1 className="text-3xl font-bold text-transparent bg-clip-text drop-shadow-2xl bg-white">
+              {courseData.title}
+            </h1>
+            <p className="text-gray-300 bg-gray-900/80 rounded-md p-2 text-sm max-w-4xl mt-auto">
+              {courseData.description}
+            </p>
           </div>
-          <p className="text-lg font-bold text-gray-700/90">
-            Choose number of lessons per week:
-          </p>
-          <div className="flex flex-col gap-2" ref={parent}>
-            {lessonPackage?.types?.map(
-              (lessonPackageType) =>
-                lessonPackageType.length === selectedLength && (
-                  <div key={lessonPackageType.id}>
-                    <input
-                      type="radio"
-                      id={lessonPackageType.id}
-                      name="package"
-                      value={lessonPackageType.id}
-                      onChange={() => setSelectedType(lessonPackageType)}
-                      className="hidden peer"
-                    />
-                    <label
-                      className="flex flex-row justify-between items-center bg-gray-100/80 backdrop-blur-md text-gray-700/90 backdrop-saturate-200 border-gray-100/40 border-2 py-2 px-4 rounded-xl hover:border-purple-300 duration-300 peer-checked:border-purple-600 hover:shadow-gray-900/5 hover:shadow-xl"
-                      htmlFor={lessonPackageType.id}
-                    >
-                      <div className="flex flex-col sm:flex-row flex-wrap justify-between w-full sm:items-center gap-1">
-                        <div className="w-full">
-                          <p className="text-lg font-bold w-full flex-grow">
-                            {lessonPackageType.name}
-                          </p>
-                          <p className="text-sm opacity-75">
-                            Total:{' '}
-                            {new Intl.NumberFormat('ko-KR', {
-                              style: 'currency',
-                              currency: 'KRW',
-                            }).format(lessonPackageType.price)}
-                          </p>
-                        </div>
-                        <div className="flex flex-row gap-1 leading-7 justify-end items-end">
-                          <span className="text-2xl font-bold">
-                            {new Intl.NumberFormat('ko-KR', {
-                              style: 'currency',
-                              currency: 'KRW',
-                            }).format(
-                              Math.round(
-                                lessonPackageType.price /
-                                  lessonPackageType.lessons_per_week,
-                              ),
-                            )}
+          <hr className="border-gray-400/50 rounded-full border md:hidden" />
+          <form
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
+            className="w-full flex flex-col gap-3"
+          >
+            <p className="text-lg font-bold text-gray-700/90">
+              Choose the duration:
+            </p>
+            <div className="flex gap-2">
+              {uniqueLength?.map((length) => (
+                <div key={length}>
+                  <input
+                    type="radio"
+                    id={'length' + length}
+                    name="duration"
+                    value={'length' + length}
+                    defaultChecked={selectedLength === length}
+                    className="hidden peer"
+                    onChange={() => {
+                      setSelectedLength(length);
+                      setSelectedPackage(null);
+                    }}
+                  />
+                  <label
+                    className="flex flex-row justify-between items-center bg-gray-100/80 backdrop-blur-md text-gray-700/90 backdrop-saturate-200 border-gray-100/40 border-2 py-2 px-4 rounded-xl hover:border-purple-300 duration-300 peer-checked:border-purple-600 hover:shadow-gray-900/5 hover:shadow-xl"
+                    htmlFor={'length' + length}
+                  >
+                    <div className="flex flex-col">
+                      <p className="text-lg font-bold">{length} minutes</p>
+                    </div>
+                  </label>
+                </div>
+              ))}
+            </div>
+            <p className="text-lg font-bold text-gray-700/90">
+              Choose number of lessons per week:
+            </p>
+            <div className="flex flex-col gap-2" ref={parent}>
+              {courseData?.packages?.map(
+                (pkg) =>
+                  pkg.sessionTime === selectedLength && (
+                    <div key={pkg.id}>
+                      <input
+                        type="radio"
+                        id={'package' + pkg.id}
+                        name="package"
+                        value={'package' + pkg.id}
+                        onChange={() => {
+                          setSelectedPackage(pkg);
+                        }}
+                        defaultChecked={selectedPackage === pkg}
+                        className="hidden peer"
+                      />
+                      <label
+                        className="flex flex-row justify-between items-center bg-gray-100/80 backdrop-blur-md text-gray-700/90 backdrop-saturate-200 border-gray-100/40 border-2 py-2 px-4 rounded-xl hover:border-purple-300 duration-300 peer-checked:border-purple-600 hover:shadow-gray-900/5 hover:shadow-xl"
+                        htmlFor={'package' + pkg.id}
+                      >
+                        {pkg.discount > 0 && (
+                          <span className="absolute text-xs -right-4 -top-2 bg-purple-600 p-1 rounded-full aspect-square flex items-center justify-center text-white font-bold">
+                            {' '}
+                            -{pkg.discount}%
                           </span>
-                          / <span>Lesson</span>
+                        )}
+                        <div className="flex sm:flex-row flex-wrap justify-between w-full sm:items-center gap-1">
+                          <div>
+                            <p className="text-2xl font-bold w-full flex-grow">
+                              {pkg.period}{' '}
+                              {pkg.period === 1 ? 'month' : 'months'}
+                            </p>
+                            <p className="text-sm opacity-75">
+                              {pkg.sessionsPerWeek}{' '}
+                              {pkg.sessionsPerWeek === 1 ? 'time' : 'times'} a
+                              week
+                            </p>
+                            <p className="text-sm opacity-75">
+                              {pkg.totalSessions} lessons
+                            </p>
+                            <p className="text-sm opacity-75">
+                              {new Intl.NumberFormat('ko-KR', {
+                                style: 'currency',
+                                currency: 'KRW',
+                              }).format(
+                                Math.round(
+                                  (pkg.price * (1 - pkg.discount / 100)) /
+                                    pkg.totalSessions,
+                                ),
+                              )}{' '}
+                              per lesson
+                            </p>
+                          </div>
+                          {pkg?.discount ? (
+                            <div className="flex flex-col items-center">
+                              <div className="text-lg opacity-80 font-bold line-through">
+                                {new Intl.NumberFormat('ko-KR', {
+                                  style: 'currency',
+                                  currency: 'KRW',
+                                }).format(pkg.price)}
+                              </div>
+                              <div className="text-2xl font-bold">
+                                {new Intl.NumberFormat('ko-KR', {
+                                  style: 'currency',
+                                  currency: 'KRW',
+                                }).format(pkg.price * (1 - pkg.discount / 100))}
+                              </div>
+                            </div>
+                          ) : (
+                            <div className="text-2xl font-bold">
+                              {new Intl.NumberFormat('ko-KR', {
+                                style: 'currency',
+                                currency: 'KRW',
+                              }).format(pkg.price * (1 - pkg.discount / 100))}
+                            </div>
+                          )}
                         </div>
-                      </div>
-                    </label>
-                  </div>
-                ),
-            )}
-          </div>
-          <button className="bg-purple-600 cursor-pointer rounded-xl font-bold text-white py-2 max-w-[16rem] justify-center self-end w-full flex flex-row gap-2 items-center hover:brightness-75 duration-200">
-            Proceed to checkout
-            <ArrowBack className="brightness-0 invert rotate-180 scale-125" />
-          </button>
-        </form>
+                      </label>
+                    </div>
+                  ),
+              )}
+            </div>
+            <button
+              className="bg-purple-600 cursor-pointer rounded-xl font-bold text-white py-2 max-w-[16rem] justify-center self-end w-full flex flex-row gap-2 items-center hover:brightness-75 duration-200 disabled:bg-gray-500 disabled:cursor-not-allowed"
+              disabled={selectedPackage === null}
+            >
+              Proceed to checkout
+              <ArrowBack className="brightness-0 invert rotate-180 scale-125" />
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
