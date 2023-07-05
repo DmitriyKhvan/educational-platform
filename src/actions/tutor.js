c8842a10f4cb54d1426c847a2a8dc66ed6f31b3c
Laroikin <said24abdi04@gmail.com>
2023-07-05 18:39:20 +0900

    feat: remove unused tutor code

--- src/actions/tutor.js
+++ src/actions/tutor.js
@@ -70,29 +70,6 @@ export function getTutorList(from, timezone = null) {
   }
 }
 
-export function updateTutorInfo(data) {
-  return (dispatch) => {
-    dispatch(request());
-    return TutorApi.updateTutorInfo(data)
-      .then((resp) => {
-        return dispatch(success(resp.data));
-      })
-      .catch((error) => {
-        return dispatch(failure(error.reseponse?.data));
-      });
-  };
-
-  function request() {
-    return { type: ActionTypes.UPDATE_TUTOR_INFO.REQUEST };
-  }
-  function success(data) {
-    return { type: ActionTypes.UPDATE_TUTOR_INFO.SUCCESS, payload: data };
-  }
-  function failure(error) {
-    return { type: ActionTypes.UPDATE_TUTOR_INFO.FAILURE, payload: error };
-  }
-}
-
 export function updateTutorAvailability(availabilities, tutor_id) {
   return (dispatch) => {
     dispatch(request());
@@ -148,78 +125,5 @@ export function updateExceptionDates(exceptionDates, tutor_id) {
       type: ActionTypes.UPDATE_TUTOR_EXCEPTIONDATES.FAILURE,
       payload: error,
     };
-  }
-}
-
-export function getOverallStatus(params = {}) {
-  return (dispatch) => {
-    dispatch(request());
-    return TutorApi.getOverallStatus(params)
-      .then((res) => dispatch(success(res.data)))
-      .catch((error) => dispatch(failure(error.response?.data)));
-  };
-
-  function request() {
-    return { type: ActionTypes.GET_TUTOR_OVERALL_SUMMARY.REQUEST };
-  }
-  function success(data) {
-    return {
-      type: ActionTypes.GET_TUTOR_OVERALL_SUMMARY.SUCCESS,
-      payload: data,
-    };
-  }
-  function failure(error) {
-    return {
-      type: ActionTypes.GET_TUTOR_OVERALL_SUMMARY.FAILURE,
-      payload: error,
-    };
-  }
-}
-
-export function getPaymentHistory(params = {}) {
-  return (dispatch) => {
-    dispatch(request());
-    return TutorApi.getPaymentHistory(params)
-      .then((res) => dispatch(success(res.data)))
-      .catch((error) => dispatch(failure(error.response?.data)));
-  };
-
-  function request() {
-    return { type: ActionTypes.GET_TUTOR_PAYMENT_HISTORY.REQUEST };
-  }
-  function success(data) {
-    return {
-      type: ActionTypes.GET_TUTOR_PAYMENT_HISTORY.SUCCESS,
-      payload: data,
-    };
-  }
-  function failure(error) {
-    return {
-      type: ActionTypes.GET_TUTOR_PAYMENT_HISTORY.FAILURE,
-      payload: error,
-    };
-  }
-}
-
-export function deleteTutor(id) {
-  return (dispatch) => {
-    dispatch(request());
-    return TutorApi.delete(id)
-      .then((resp) => {
-        return dispatch(success(resp.data));
-      })
-      .catch((error) => {
-        return dispatch(failure(error.response?.data));
-      });
-  };
-
-  function request() {
-    return { type: ActionTypes.DELETE_TUTOR.REQUEST };
-  }
-  function success(data) {
-    return { type: ActionTypes.DELETE_TUTOR.SUCCESS, payload: data };
-  }
-  function failure(error) {
-    return { type: ActionTypes.DELETE_TUTOR.FAILURE, payload: error };
   }
 }
