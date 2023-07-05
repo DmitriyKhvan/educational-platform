d538982e44364e0193a32c63d354cc4d895d5694
Laroikin <said24abdi04@gmail.com>
2023-07-05 19:13:51 +0900

    feat: remove unused actions of students & subscriptions

--- src/actions/students.js
+++ src/actions/students.js
@@ -1,52 +1,5 @@
 import StudentApi from '../api/StudentApi';
-import AdminApi from '../api/AdminApi';
 import ActionTypes from '../constants/actionTypes';
-
-export function getStudent(student_id) {
-  return (dispatch) => {
-    dispatch(request());
-    return StudentApi.getStudent(student_id)
-      .then((resp) => {
-        return dispatch(success(resp.data));
-      })
-      .catch((error) => {
-        return dispatch(failure(error.response?.data));
-      });
-  };
-
-  function request() {
-    return { type: ActionTypes.GET_STUDENT_INFO.REQUEST };
-  }
-  function success(data) {
-    return { type: ActionTypes.GET_STUDENT_INFO.SUCCESS, payload: data };
-  }
-  function failure(error) {
-    return { type: ActionTypes.GET_STUDENT_INFO.FAILURE, payload: error };
-  }
-}
-
-export function updateStudent(data) {
-  return (dispatch) => {
-    dispatch(request());
-    return StudentApi.updateStudent(data)
-      .then((resp) => {
-        return dispatch(success(resp.data));
-      })
-      .catch((error) => {
-        return dispatch(failure(error.response?.data));
-      });
-  };
-
-  function request() {
-    return { type: ActionTypes.UPDATE_STUDENT_PROFILE.REQUEST };
-  }
-  function success(data) {
-    return { type: ActionTypes.UPDATE_STUDENT_PROFILE.SUCCESS, payload: data };
-  }
-  function failure(error) {
-    return { type: ActionTypes.UPDATE_STUDENT_PROFILE.FAILURE, payload: error };
-  }
-}
 
 export function setFavourite(data) {
   return (dispatch) => {
@@ -70,30 +23,6 @@ export function setFavourite(data) {
   }
 }
 
-export function fetchFavoriteTutors(data = {}) {
-  data.favorite = true;
-
-  return (dispatch) => {
-    dispatch(request());
-    return AdminApi.fetchTutors(data)
-      .then((resp) => {
-        return dispatch(success(resp.data));
-      })
-      .catch((error) => {
-        return dispatch(failure(error.response?.data));
-      });
-  };
-
-  function request() {
-    return { type: ActionTypes.FETCH_FAVORITE_TUTORS.REQUEST, payload: data };
-  }
-  function success(data) {
-    return { type: ActionTypes.FETCH_FAVORITE_TUTORS.SUCCESS, payload: data };
-  }
-  function failure(error) {
-    return { type: ActionTypes.FETCH_FAVORITE_TUTORS.FAILURE, payload: error };
-  }
-}
 
 export function fetchStudentList(params = {}) {
   return (dispatch) => {
