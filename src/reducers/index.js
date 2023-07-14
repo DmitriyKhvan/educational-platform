0094c2e002df22d56bf7795ecc0f8edc0b2e5cc7
Laroikin <said24abdi04@gmail.com>
2023-07-14 09:48:04 +0500

    feat: remove subscription actions & reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import tutor from './tutor';
-import subscription from './subscription';
 import appointment from './appointment';
 
 export default combineReducers({
   tutor,
-  subscription,
   appointment,
 });
