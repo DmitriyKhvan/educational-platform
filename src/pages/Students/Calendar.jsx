34009f032fdee373acbab728262c37549567d1b1
Laroikin <48063190+Laroikin@users.noreply.github.com>
2023-08-03 17:05:47 +0500

    fix: remove unused lesson query

--- src/pages/Students/Calendar.jsx
+++ src/pages/Students/Calendar.jsx
@@ -18,8 +18,6 @@ import FeedbackLessonModal from '../Mentors/FeedbackLessonModal';
 import WeekHeader from '../../components/common/WeekHeader';
 import { useQuery, useMutation } from '@apollo/client';
 import {
-  MENTORS_QUERY,
-  LESSON_QUERY,
   APPOINTMENTS_QUERY,
   CANCEL_APPOINTMENT,
 } from '../../modules/auth/graphql';
@@ -98,18 +96,6 @@ const sortCalendarEvents = (data) => {
 };
 
 const Calendar = () => {
-  const [idOfLesson, setIdLesson] = React.useState(null);
-  const { data } = useQuery(LESSON_QUERY, {
-    variables: { id: idOfLesson },
-  });
-  const { data: mentorsList } = useQuery(MENTORS_QUERY, {
-    errorPolicy: 'ignore',
-  });
-
-  const mentors = mentorsList?.mentors?.find(
-    (i) => +i?.id === +data?.lesson?.mentor?.id,
-  );
-
   const [t] = useTranslation(['lessons']);
   const location = useLocation();
   const { user } = useAuth();
@@ -226,10 +212,6 @@ const Calendar = () => {
     const [selectedEvent] =
       calendarEvents?.filter((x) => x.id === calendarEvent.id) ?? [];
 
-    if (selectedEvent) {
-      setIdLesson(selectedEvent.resource?.eventDate?.id);
-    }
-
     const scheduledTime = moment(selectedEvent?.resource?.startAt).tz(
       userTimezone,
     );
@@ -249,10 +231,8 @@ const Calendar = () => {
           contentLabel="Example Modal"
         >
           <CalendarModal
-            setIdLesson={setIdLesson}
             event={selectedEvent}
             lesson={selectedEvent?.title}
-            mentors={mentors}
             startTime={startTime}
             endTime={endTime}
             zoomlink={selectedEvent.resource?.zoomLink}
