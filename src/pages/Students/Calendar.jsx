b2f6d9b80a34320c2745a00b512b2f37177c6536
kodir <amirovqodir911@gmail.com>
2023-08-14 08:04:32 +0500

    #502 past lessens - gray color

--- src/pages/Students/Calendar.jsx
+++ src/pages/Students/Calendar.jsx
@@ -328,7 +328,9 @@ const Calendar = () => {
 
   const eventPropGetter = useCallback((event) => {
     return {
-      ...(event.resource.status === 'scheduled' && {
+      ...(event.resource.status === 'scheduled'
+        || new Date(event.resource.startAt) < new Date()
+        && {
         style: {
           background: 'none',
           backgroundColor: '#909090',
