e98abc1ec2ad3d8f70c6d5d6e3ade8fe319bd61b
freedom_twin <dimon.twin@gmail.com>
2023-10-30 10:45:13 +0500

    fix: change subscription method

--- src/utils/subscriptions.js
+++ src/utils/subscriptions.js
@@ -2,33 +2,35 @@ import { createClient } from 'graphql-ws';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { gql, useSubscription } from '@apollo/client';
 
-export const MESSAGE_SUBSCRIPTIONS = gql`subscription OnNewMessages {
+export const MESSAGE_SUBSCRIPTIONS = gql`
+  subscription OnNewMessages {
     newMessages {
+      id
+      body
+      meta
+      sender {
         id
-        body
-        meta
-        sender {
-            id
-            email
-            firstName
-            lastName
-            fullName
-            koreanEquivalent
-            phoneNumber
-            address
-            gender
-            timeZone
-            country
-            avatar
-            referalCode
-            referalId
-            isActive
-            role
-            createdAt
-            updatedAt
-        }
+        email
+        #firstName
+        #lastName
+        #fullName
+        #koreanEquivalent
+        phoneNumber
+        address
+        #gender
+        timeZone
+        country
+        #avatar
+        referalCode
+        referalId
+        isActive
+        role
+        createdAt
+        updatedAt
+      }
     }
-}`
+  }
+`;
 export const createWsLink = function (url) {
   const client = new GraphQLWsLink(
     createClient({
