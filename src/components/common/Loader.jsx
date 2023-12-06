c418ece33ce2d1b24e5d3fd3269c750c31716d46
freedom_twin <dimon.twin@gmail.com>
2023-12-06 12:48:32 +0500

    fix: available slote validation

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -3,26 +3,25 @@ import Loader from 'react-loader-spinner';
 
 const ReactLoader = ({ className = 'absolute' }) => {
   return (
-    <div className="relative w-full h-full">
-      <Loader
-        color="#00BFFF"
-        className={`
-        w-full 
-        h-full 
+    <Loader
+      color="#00BFFF"
+      className={`
+        fixed
         bg-slate-400/20 
         z-[10000] 
         top-0 
         left-0 
+        bottom-0
+        right-0
         flex 
         items-center 
         justify-center
         ${className}
         `}
-        type="TailSpin"
-        height={80}
-        width={80}
-      />
-    </div>
+      type="TailSpin"
+      height={80}
+      width={80}
+    />
   );
 };
 
