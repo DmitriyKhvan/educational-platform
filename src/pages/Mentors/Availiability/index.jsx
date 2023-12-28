7581e571de727d93175a2c9c93c5a3cbdd8677ce
freedom_twin <dimon.twin@gmail.com>
2023-12-28 16:34:22 +0500

    layout

--- src/pages/Mentors/Availiability/index.jsx
+++ src/pages/Mentors/Availiability/index.jsx
@@ -1,11 +1,14 @@
 // import '../../../assets/styles/availability.scss';
 import Layout from '../../../components/Layout';
 import Availability from './Availability';
+import { AvailabilityExceptions } from './AvailabilityExceptions';
 
 const AvailiabilityLayout = () => {
   return (
     <Layout>
       <Availability />
+      <div className="divider"></div>
+      <AvailabilityExceptions />
     </Layout>
   );
 };
