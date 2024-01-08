7eacc4803e80cc4b8eac774248a54db397f08081
freedom_twin <dimon.twin@gmail.com>
2024-01-08 17:16:42 +0500

    HP-297: Past lesson tab on calendar sort desc

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     handleSubmit,
     formState: { errors, isValid },
   } = useForm({
-    mode: 'all',
+    mode: 'onChange',
     defaultValues: {
       email: '',
       password: '',
