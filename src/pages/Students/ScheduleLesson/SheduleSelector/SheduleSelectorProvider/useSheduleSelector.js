d1cd82f10437deca7e23b96841657ca918d2ac0b
freedom_twin <dimon.twin@gmail.com>
2024-02-07 10:00:47 +0500

    remove context

--- src/pages/Students/ScheduleLesson/SheduleSelector/SheduleSelectorProvider/useSheduleSelector.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { SheduleSelectorContext } from './SheduleSelectorContext';
-
-export const useSheduleSelector = () => {
-  return useContext(SheduleSelectorContext);
-};
