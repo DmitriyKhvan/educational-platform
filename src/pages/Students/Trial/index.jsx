ded7f08254e073f1faede74843f2d64445cc51a4
igor <pythagoras1206@gmail.com>
2024-02-26 18:09:08 +0900

    feat: day/time select

--- src/pages/Students/Trial/index.jsx
+++ src/pages/Students/Trial/index.jsx
@@ -6,9 +6,14 @@ import StepIndicator from './StepIndicator';
 import SelectDay from './SelectDay';
 import AvailableSpots from './AvailableSpots';
 import Confirmation from './Confirmation';
+import { ScheduleProvider } from '../ScheduleLesson/ScheduleSelector/ScheduleProvider';
+// import { ScheduleSelector } from '../ScheduleLesson/ScheduleSelector';
+// import { AvailableTimes } from '../ScheduleLesson/ScheduleSelector/AvailableTimes';
 
 const Trial = () => {
   const [step, setStep] = useState(0);
+  // const [selectedPlan, setSelectedPlan] = useState({});
+  // const [schedule, setSchedule] = useState();
 
   return (
     <OnboardingLayout>
@@ -16,8 +21,19 @@ const Trial = () => {
         {step > 0 && <StepIndicator step={step} />}
         {step === 0 && <OnboardingTrial setStep={setStep} />}
         {step === 1 && <LessonDetails setStep={setStep} />}
-        {step === 2 && <SelectDay setStep={setStep} />}
-        {step === 3 && <AvailableSpots setStep={setStep} />}
+
+        <ScheduleProvider
+          setTabIndex={setStep}
+          setSchedule={() => {}}
+          // selectedMentor={location?.state?.tutor}
+          duration={25}
+        >
+          {step === 2 && <SelectDay setStep={setStep} />}
+
+          {step === 3 && <AvailableSpots setStep={setStep} />}
+        </ScheduleProvider>
+        {/* {step === 2 && <SelectDay setStep={setStep} />}
+        {step === 3 && <AvailableSpots setStep={setStep} />} */}
         {step === 4 && <Confirmation setStep={setStep} />}
       </main>
     </OnboardingLayout>
