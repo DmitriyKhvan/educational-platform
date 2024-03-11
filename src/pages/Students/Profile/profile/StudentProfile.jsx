bb7165fa8ac05fd7b8e96d42611c4f002f00bbc1
igor <pythagoras1206@gmail.com>
2024-03-11 11:47:28 +0900

    fix: button adjustment

--- src/pages/Students/Profile/profile/StudentProfile.jsx
+++ src/pages/Students/Profile/profile/StudentProfile.jsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useAuth } from '../../../../modules/auth';
-import { useMutation, useQuery } from '@apollo/client';
-import {
-  MUTATION_UPDATE_STUDENT,
-  PACKAGE_QUERY,
-} from '../../../../modules/auth/graphql';
+import { useQuery } from '@apollo/client';
+import { PACKAGE_QUERY } from '../../../../modules/auth/graphql';
 import { useTranslation } from 'react-i18next';
-import Introduction from './Introduction';
-import Button from '../../../../components/Form/Button/Button';
 import { Avatar } from '../../../../widgets/Avatar/Avatar';
-import notify from '../../../../utils/notify';
-import ModalWrapper from '../../../../components/ModalWrapper/ModalWrapper';
-import EditProflileStudent from '../editInfo/EditStudentProfile';
-import ReactLoader from '../../../../components/common/Loader';
-import { LessonsStatusType, getItemToLocalStorage } from 'src/constants/global';
+import { getItemToLocalStorage } from 'src/constants/global';
+import { Link } from 'react-router-dom/cjs/react-router-dom';
+import { FaPencil } from 'react-icons/fa6';
+import Button from 'src/components/Form/Button';
+import { FiLogOut } from 'react-icons/fi';
+import { AdaptiveDialog } from 'src/components/AdaptiveDialog';
+import LevelBadge from './LevelBadge';
+import PackageCard from './PackageCard';
 
 const StudentProfile = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [logoutOpen, setLogoutOpen] = useState(false);
 
-  const [t] = useTranslation(['profile', 'common', 'lessons']);
+  const [t] = useTranslation(['profile', 'common', 'lessons', 'modals']);
 
-  const [updateStudent] = useMutation(MUTATION_UPDATE_STUDENT);
+  const { user, currentStudent, logout } = useAuth();
 
-  const { user, refetchUser, currentStudent } = useAuth();
+  const handleLogout = async () => {
+    await logout();
+    window.Intercom('shutdown');
+    window.location.reload(true);
+  };
 
   const { data: { packageSubscriptions: planStatus } = {} } = useQuery(
     PACKAGE_QUERY,
@@ -34,199 +35,202 @@ const StudentProfile = () => {
     },
   );
 
-  const saveIntroduction = async (text) => {
-    if (text !== '') {
-      const { data } = await updateStudent({
-        variables: {
-          // id: parseInt(user?.student?.id),
-          id: parseInt(getItemToLocalStorage('studentId')),
-          data: {
-            about: text,
-          },
-        },
-      });
+  const [uncompletedPackages, completedPackages] = useMemo(
+    () => [
+      planStatus?.filter((x) => x.credits),
+      planStatus?.filter((x) => !x.credits),
+    ],
+    [planStatus],
+  );
 
-      if (data) {
-        notify(t('introduction_changed', { ns: 'profile' }));
-      }
-
-      await refetchUser();
-    }
-  };
-
-  const getCompleteLessons = (lessons) => {
-    return lessons.filter(
-      (lesson) =>
-        lesson.status === LessonsStatusType.COMPLETED ||
-        lesson.status === LessonsStatusType.PAID,
-    ).length;
-  };
+  console.log(completedPackages);
 
   return (
     <>
-      {loading && <ReactLoader className="fixed !important" />}
-      <div className="flex flex-wrap h-[calc(100vh-80px)] overflow-auto ">
-        <div className="sm:w-full xl:w-3/5 p-[30px] sm:p-[60px]">
-          <div>
-            <div className="relative w-full h-[150px] bg-color-purple rounded-t-[10px]">
-              <div className="absolute left-[5%] top-[55%] w-[140px] h-[140px] bg-white border-8 border-solid border-white rounded-[10px]">
-                <Avatar avatarUrl={user?.avatar?.url} />
+      <div className="flex flex-col min-h-[calc(100vh-120px)] mt-10">
+        <div className="w-full px-6 max-w-[448px] mx-auto space-y-8">
+          <header className="flex items-center w-full space-x-4 ">
+            <div className="w-16 h-16 sm:w-20 sm:h-20">
+              <Avatar
+                avatarUrl={user?.avatar?.url}
+                className="w-full h-full min-w-16 sm:min-w-20 rounded-full border"
+              />
+            </div>
+            <div className="w-full h-auto py-2">
+              <div className="flex justify-between ml-auto w-full">
+                <div className="flex justify-between w-full">
+                  <div>
+                    <h2 className="text-[20px] sm:text-[24px] font-bold leading-9 tracking-[-1px] text-color-dark-purple">
+                      {user?.firstName + ' '}
+                      {user?.lastName}
+                    </h2>
+                    <Link
+                      to="/student/profile/edit"
+                      className="text-color-purple flex items-center gap-2 hover:underline font-medium text-[13px] sm:text-sm"
+                    >
+                      <FaPencil /> {t('edit_profile')}
+                    </Link>
+                  </div>
+
+                  <div>
+                    <LevelBadge level={currentStudent?.langLevel} />
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="w-full h-auto py-4 border border-solid border-color-border-grey rounded-b-[10px]">
-              <div className="flex justify-between w-[65%] ml-auto">
-                <div>
-                  <h2 className="mt-0 mb-[10px] text-3xl leading-9 tracking-[-1px] text-color-dark-purple">
-                    {user?.firstName + ' '}
-                    {user?.lastName}
-                  </h2>
+          </header>
 
-                  <p className="text-base font-semibold tracking-[-0.6px] text-color-purple">
-                    {currentStudent?.langLevel}
+          <div className="w-full ">
+            <h2 className="text-[20px] font-bold text-color-dark-purple tracking-[-0.6px] leading-6">
+              {t('add_details')}
+            </h2>
+
+            <div className="flex flex-col gap-5 mt-5">
+              <div className="flex justify-between border-b">
+                <h4 className="font-medium text-sm text-gray-400 leading-6 tracking-[-0.6px] mb-[10px]">
+                  {t('email')}
+                </h4>
+                <p className="font-medium text-sm text-color-dark-purple leading-6 tracking-[-0.6px]">
+                  {user?.email}
+                </p>
+              </div>
+
+              {user?.koreanEquivalent && (
+                <div className="flex justify-between border-b">
+                  <h4 className="font-medium text-sm text-gray-400 leading-6 tracking-[-0.6px] mb-[10px]">
+                    {t('korean_name')}
+                  </h4>
+                  <p className="font-medium text-sm text-color-dark-purple leading-6 tracking-[-0.6px]">
+                    {user?.koreanEquivalent}
                   </p>
+                </div>
+              )}
 
-                  <p className="text-base font-semibold tracking-[-0.6px] text-color-light-grey">
+              {user?.gender && (
+                <div className="flex justify-between border-b">
+                  <h4 className="font-medium text-sm text-gray-400 leading-6 tracking-[-0.6px] mb-[10px]">
+                    {t('gender')}
+                  </h4>
+                  <p className="font-medium text-sm text-color-dark-purple  leading-6 tracking-[-0.6px]">
+                    {user?.gender}
+                  </p>
+                </div>
+              )}
+
+              {user?.country && (
+                <div className="flex justify-between border-b">
+                  <h4 className="font-medium text-sm text-gray-400 leading-6 tracking-[-0.6px] mb-[10px]">
+                    {t('country', { ns: 'common' })}
+                  </h4>
+                  <p className="font-medium text-sm text-color-dark-purple leading-6 tracking-[-0.6px]">
+                    {user?.country ? user?.country : 'Korea'}
+                  </p>
+                </div>
+              )}
+
+              {user?.address && (
+                <div className="flex justify-between border-b">
+                  <h4 className="font-medium text-sm text-gray-400 leading-6 tracking-[-0.6px] mb-[10px]">
+                    {t('address', { ns: 'common' })}
+                  </h4>
+                  <p className="font-medium text-sm text-color-dark-purple leading-6 tracking-[-0.6px]">
+                    {user?.address}
+                  </p>
+                </div>
+              )}
+
+              {user?.phoneNumber && (
+                <div className="flex justify-between border-b">
+                  <h4 className="font-medium text-sm text-gray-400 leading-6 tracking-[-0.6px] mb-[10px]">
+                    {t('phone_number', { ns: 'common' })}
+                  </h4>
+                  <p className="font-medium text-sm text-color-dark-purple leading-6 tracking-[-0.6px]">
+                    {user?.phoneNumber}
+                  </p>
+                </div>
+              )}
+
+              {user?.timeZone && (
+                <div className="flex justify-between border-b">
+                  <h4 className="font-medium text-sm text-gray-400 leading-6 tracking-[-0.6px] mb-[10px]">
+                    {t('time_zone', { ns: 'common' })}
+                  </h4>
+                  <p className="font-medium text-sm text-color-dark-purple leading-6 tracking-[-0.6px]">
                     {user?.timeZone ? user?.timeZone : 'PST (GMT-8)'}
                   </p>
                 </div>
-
-                <div>
-                  <Button
-                    className="mr-4"
-                    theme="outline"
-                    onClick={() => {
-                      setIsOpen(true);
-                    }}
-                  >
-                    {t('edit_profile')}
-                  </Button>
-                </div>
-              </div>
+              )}
             </div>
-          </div>
-
-          <Introduction
-            text={currentStudent.about}
-            onChange={saveIntroduction}
-          />
-
-          <div className="mt-[30px]">
-            <h2 className="text-xl text-color-dark-purple font-medium tracking-[-0.6px] leading-6">
-              {t('enrolled_courses')}
-            </h2>
-            {planStatus &&
-              planStatus.map((item) => (
-                <div
-                  key={item.id}
-                  className="flex items-center gap-5 mt-5 p-5 bg-white border border-solid border-color-border-grey rounded-[10px]"
-                >
-                  <h3 className="text-sm text-color-dark-purple font-semibold leading-6 tracking-[-0.6px] whitespace-nowrap">
-                    {item.package.course.title}
-                  </h3>
-
-                  <Button className="px-[10px] cursor-auto" theme="purple">
-                    {t('lesson_type', { ns: 'profile' })}
-                  </Button>
-
-                  <Button className="px-[10px] text-color-purple bg-color-light-purple cursor-auto">
-                    {item.package.sessionTime} {t('minutes', { ns: 'common' })}
-                  </Button>
-
-                  <Button
-                    className="px-[10px] cursor-auto hover:bg-white hover:text-inherit"
-                    theme="outline"
-                  >
-                    {t('lessons_completed', {
-                      ns: 'lessons',
-                      count: getCompleteLessons(item.lessons),
-                      total: item.package.totalSessions,
-                    })}
-                  </Button>
-                </div>
-              ))}
-
-            {planStatus?.length === 0 && (
-              <p className="mt-[30px]">{t('no_lessons', { ns: 'lessons' })}</p>
-            )}
           </div>
         </div>
 
-        <div className="w-full xl:w-2/5 p-[30px] sm:p-[60px] border-t xl:border-t-0 xl:border-l border-solid border-color-border-grey bg-[#F7F7FA]">
-          <h2 className="font-medium text-2xl text-color-light-grey tracking-[-0.6px]">
-            {t('add_details')}
+        <div className="mt-[30px] max-w-[448px] px-6 w-full mx-auto">
+          <h2 className="mb-4 text-[20px] font-bold text-color-dark-purple tracking-[-0.6px] leading-6">
+            {t('enrolled_courses')}
           </h2>
+          {uncompletedPackages &&
+            uncompletedPackages.map((item) => (
+              <PackageCard key={item.id} item={item} />
+            ))}
 
-          <div className="flex flex-col gap-5 mt-5">
-            <div>
-              <h4 className="font-medium text-xl text-color-dark-purple leading-6 tracking-[-0.6px] mb-[10px]">
-                {t('email')}
-              </h4>
-              <p className="font-medium text-xl text-color-purple leading-6 tracking-[-0.6px]">
-                {user?.email}
+          {uncompletedPackages?.length === 0 && (
+            <p className="mt-[30px]">{t('no_lessons', { ns: 'lessons' })}</p>
+          )}
+        </div>
+
+        <div className="mt-[30px] max-w-[448px] px-6 w-full mx-auto">
+          <h2 className="mb-4 text-[20px] font-bold text-color-dark-purple tracking-[-0.6px] leading-6">
+            {t('completed_courses')}
+          </h2>
+          {completedPackages &&
+            completedPackages.map((item) => (
+              <PackageCard key={item.id} item={item} completed />
+            ))}
+
+          {completedPackages?.length === 0 && (
+            <p className="mt-[30px]">{t('no_lessons', { ns: 'lessons' })}</p>
+          )}
+        </div>
+
+        <div className="px-6">
+          <AdaptiveDialog
+            button={
+              <Button
+                theme="red"
+                className="max-w-[400px] w-full mx-auto my-8 h-[60px]"
+              >
+                <FiLogOut className="mr-2" /> Log out
+              </Button>
+            }
+            open={logoutOpen}
+            setOpen={setLogoutOpen}
+          >
+            <div className="text-center">
+              <h2 className="font-bold text-[22px] mb-4">
+                {t('logging_out', { ns: 'modals' })}
+              </h2>
+              <p className="text-[15px] mb-6">
+                {t('are_you_sure_to_logout', { ns: 'modals' })}
               </p>
+
+              <Button
+                onClick={handleLogout}
+                theme="destructive"
+                className="w-full mb-3"
+              >
+                {t('yes_logout', { ns: 'modals' })}
+              </Button>
+              <Button
+                onClick={() => setLogoutOpen(false)}
+                theme="outline"
+                className="w-full"
+              >
+                {t('cancel', { ns: 'modals' })}
+              </Button>
             </div>
-
-            {user?.koreanEquivalent && (
-              <div>
-                <h4 className="font-medium text-xl text-color-dark-purple leading-6 tracking-[-0.6px] mb-[10px]">
-                  {t('korean_name')}
-                </h4>
-                <p className="font-medium text-xl text-color-purple leading-6 tracking-[-0.6px]">
-                  {user?.koreanEquivalent}
-                </p>
-              </div>
-            )}
-
-            {user?.gender && (
-              <div>
-                <h4 className="font-medium text-xl text-color-dark-purple leading-6 tracking-[-0.6px] mb-[10px]">
-                  {t('gender')}
-                </h4>
-                <p className="font-medium text-xl text-color-purple leading-6 tracking-[-0.6px]">
-                  {user?.gender}
-                </p>
-              </div>
-            )}
-
-            {user?.country && (
-              <div>
-                <h4 className="font-medium text-xl text-color-dark-purple leading-6 tracking-[-0.6px] mb-[10px]">
-                  {t('country', { ns: 'common' })}
-                </h4>
-                <p className="font-medium text-xl text-color-purple leading-6 tracking-[-0.6px]">
-                  {user?.country ? user?.country : 'Korea'}
-                </p>
-              </div>
-            )}
-
-            {user?.address && (
-              <div>
-                <h4 className="font-medium text-xl text-color-dark-purple leading-6 tracking-[-0.6px] mb-[10px]">
-                  {t('address', { ns: 'common' })}
-                </h4>
-                <p className="font-medium text-xl text-color-purple leading-6 tracking-[-0.6px]">
-                  {user?.address}
-                </p>
-              </div>
-            )}
-
-            {user?.phoneNumber && (
-              <div>
-                <h4 className="font-medium text-xl text-color-dark-purple leading-6 tracking-[-0.6px] mb-[10px]">
-                  {t('phone_number', { ns: 'common' })}
-                </h4>
-                <p className="font-medium text-xl text-color-purple leading-6 tracking-[-0.6px]">
-                  {user?.phoneNumber}
-                </p>
-              </div>
-            )}
-          </div>
+          </AdaptiveDialog>
         </div>
       </div>
-
-      <ModalWrapper isOpen={isOpen} closeModal={setIsOpen}>
-        <EditProflileStudent closeModal={setIsOpen} setLoading={setLoading} />
-      </ModalWrapper>
     </>
   );
 };
