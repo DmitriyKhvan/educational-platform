8654c5c120072f1e3faffdda60c22687f4f105bf
Ownsk <k.b.608@mail.ru>
2024-04-24 14:53:31 +0900

    fixed graphql query

--- src/modules/graphql/queries/trial/combinedTimesheetsForTrials.js
+++ src/modules/graphql/queries/trial/combinedTimesheetsForTrials.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const COMBINED_TIMESHEETS_TRIAL = gql`
-  query combinedTimesheetsForTrials($tz: String!, $date: String!) {
-    combinedTimesheetsForTrials(tz: $tz, date: $date) {
+  query combinedTimesheetsForTrials($tz: String!, $date: String!, $mentorId: ID) {
+    combinedTimesheetsForTrials(tz: $tz, date: $date, mentorId: $mentorId) {
       id
       day
       from
