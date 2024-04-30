28379c27b9a8a120976248baaea1c7a221021f96
freedom_twin <dimon.twin@gmail.com>
2024-04-30 11:03:28 +0500

    accepting new student rebranding

--- src/pages/Mentors/Availiability/AcceptingStudents.jsx
+++ src/pages/Mentors/Availiability/AcceptingStudents.jsx
@@ -1,24 +1,15 @@
 import { useMutation } from '@apollo/client';
-import { useForm } from 'react-hook-form';
-import { useTranslation } from 'react-i18next';
-import Button from 'src/components/Form/Button';
+import * as Switch from '@radix-ui/react-switch';
 import Loader from 'src/components/Loader/Loader';
 import { useAuth } from 'src/modules/auth';
 import { ACCEPT_NEW_STUDENTS } from 'src/modules/graphql/mutations/acceptNewStudents';
 import notify from 'src/utils/notify';
 
 export const AcceptingStudents = () => {
-  const [t] = useTranslation('common');
   const { user, refetchUser } = useAuth();
   const [acceptNewStudents, { loading }] = useMutation(ACCEPT_NEW_STUDENTS);
 
-  const { register, handleSubmit, watch } = useForm({
-    defaultValues: {
-      accept: user.mentor.acceptingStudents,
-    },
-  });
-
-  const submit = ({ accept }) => {
+  const submit = (accept) => {
     acceptNewStudents({
       variables: {
         mentorId: user.mentor.id,
@@ -40,32 +31,26 @@ export const AcceptingStudents = () => {
           <Loader />
         </div>
       )}
-      <div className="">
-        <form
-          className="flex items-center justify-between"
-          onSubmit={handleSubmit(submit)}
-        >
-          <div className="form-switch flex items-center py-5 px-4 border border-gray-100 rounded-lg shadow-[0px_0px_8px_0px_rgba(0,_0,_0,_0.08)]">
-            <input
-              className="form-check-input text-color-purple"
-              type="checkbox"
-              name="accept"
-              {...register('accept')}
-            />
 
-            <span className="text-sm text-color-dark-purple font-medium">
-              Accept New Students
-            </span>
-          </div>
-
-          <Button
-            type="submit"
-            disabled={user.mentor.acceptingStudents === watch('accept')}
-            className="w-[15%]"
+      <div className="flex">
+        <div className="flex items-center gap-4 py-5 px-4 border border-gray-100 rounded-lg shadow-[0px_0px_8px_0px_rgba(0,_0,_0,_0.08)]">
+          <Switch.Root
+            className="w-[40px] h-[24px] bg-gray-100 shadow-[0_0_0_1px_rgba(0,_0,_0,_0.05)] rounded-full relative outline-none cursor-pointer focus:border-[hsla(210,_96%,_45%,_50%)] focus:shadow-[0_0_0_0.25rem_rgba(13,110,253,0.25)] data-[state=checked]:bg-color-purple/10"
+            id="airplane-mode"
+            // style={{ '-webkit-tap-highlight-color': 'rgba(0, 0, 0, 0)' }}
+            defaultChecked={user.mentor.acceptingStudents}
+            onCheckedChange={submit}
           >
-            {t('save', { ns: 'common' })}
-          </Button>
-        </form>
+            <Switch.Thumb className="block w-[16px] h-[16px] bg-gray-400 rounded-full transition-transform duration-100 translate-x-1 will-change-transform data-[state=checked]:translate-x-[20px] data-[state=checked]:bg-color-purple" />
+          </Switch.Root>
+
+          <label
+            className="text-sm text-color-dark-purple font-medium"
+            htmlFor="airplane-mode"
+          >
+            Accept New Students
+          </label>
+        </div>
       </div>
     </>
   );
