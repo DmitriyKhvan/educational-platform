c154fdf3684739b263360fe3158984bcca1502b3
freedom_twin <dimon.twin@gmail.com>
2024-05-01 13:27:13 +0500

    refact: mobileMenu style

--- src/components/Menu/ui/MobileMenu/MobileMenu.jsx
+++ src/components/Menu/ui/MobileMenu/MobileMenu.jsx
@@ -4,25 +4,19 @@ import { MdMoreHoriz } from 'react-icons/md';
 import { MyDrawer } from '../../../Drawer';
 import { MobileMenuFull } from '../MobileMenuFull';
 import { MobileMenuItem } from './MobileMenuItem';
-import { cn } from 'src/utils/functions';
-import { useAuth } from 'src/modules/auth';
-import { Roles } from 'src/constants/global';
 
 export const MobileMenu = () => {
   const navLinks = useMenuList();
-  const [open, setOpen] = useState(false);
+  const minNavLinks = navLinks.slice(0, 4);
 
-  const { user } = useAuth();
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="sticky bottom-0 left-0 flex justify-center items-center w-full h-16 sm:h-20 bg-white shadow-[4px_0px_16px_0px_rgba(0,_0,_0,_0.04)]">
       <ul
-        className={cn(
-          'grid w-full sm:w-auto',
-          user.role === Roles.MENTOR ? 'grid-cols-4' : ' grid-cols-5',
-        )}
+        className={`grid w-full sm:w-auto grid-cols-${minNavLinks.length + 1}`}
       >
-        {navLinks.slice(0, 4).map((menu) => {
+        {minNavLinks.map((menu) => {
           return <MobileMenuItem key={menu.link} menu={menu} />;
         })}
         <li className="grid justify-center">
