a9b164e3a9ffebb07d6466f2190f4023cb54849e
Ownsk <k.b.608@mail.ru>
2024-05-20 17:10:42 +0900

    pass referral code over coockie

--- src/utils/referralCodeCookie.js
+++ src/utils/referralCodeCookie.js
@@ -4,3 +4,12 @@ export const getReferralCodeCookie = (document) => {
         .find((row) => row.startsWith('referralCode='))
         ?.split('=')[1];
 }
+
+export const deleteReferralCodeCookie = () => {
+    const cookies = document.cookie.split('; ');
+    const cookieList = cookies.filter((cookie) => cookie.startsWith('referralCode'));
+    cookieList.forEach((cookie) => {
+      document.cookie = cookie
+        .split('=')[0] + '=;expires=' + new Date(0).toUTCString();
+    });
+}
