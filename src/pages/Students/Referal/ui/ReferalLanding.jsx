484928d61f60351d54564d8df735630f33f759e3
igor <pythagoras1206@gmail.com>
2024-06-03 16:04:49 +0900

    feat: links refactor

--- src/pages/Students/Referal/ui/ReferalLanding.jsx
+++ src/pages/Students/Referal/ui/ReferalLanding.jsx
@@ -52,6 +52,71 @@ const ReferalLanding = () => {
     navigate('/trial');
   };
 
+  const quickLinks = [
+    {
+      title: 'Speaking Course',
+      href: 'https://www.naonow.com/curriculum',
+    },
+    {
+      title: 'Writing Course',
+      href: 'https://www.naonow.com/writing',
+    },
+    {
+      title: 'Mentors',
+      href: 'https://www.naonow.com/mentors',
+    },
+    {
+      title: 'FAQ',
+      href: 'https://www.naonow.com/faq',
+    },
+    {
+      title: 'Purchase',
+      href: 'https://www.naonow.com/onboarding',
+    },
+    {
+      title: 'Contact Us',
+      href: 'https://www.naonow.com/contact-us',
+    },
+  ];
+
+  const socialLinks = [
+    {
+      icon: <FaInstagram />,
+      href: 'https://www.instagram.com/naonowmentoring/',
+    },
+    {
+      icon: <RiKakaoTalkFill />,
+      href: 'https://pf.kakao.com/_XdGUb',
+    },
+    {
+      icon: <IoLogoFacebook />,
+      href: 'https://www.facebook.com/naonowmentoring',
+    },
+    {
+      icon: <FaLinkedin />,
+      href: 'https://www.linkedin.com/company/nao-now-mentoring',
+    },
+    {
+      icon: <SiNaver />,
+      href: 'https://blog.naver.com/naonowmentoring/',
+    },
+  ];
+
+  const contactInfo = [
+    {
+      icon: <IoMdMail className="text-xl" />,
+      text: 'support@naonow.com',
+    },
+    {
+      icon: <FaPhone className="text-xl" />,
+      text: '02-3498-6970',
+    },
+    {
+      icon: <FaMapMarkerAlt className="text-xl" />,
+      text: '325 N. Saint Paul Street, Suite 3100, Dallas, TX 75201',
+    },
+  ];
+
   return (
     <section className="bg-color-purple min-h-screen text-white relative">
       <header className="flex mb-6 justify-between items-center gap-2 px-5 py-[10px] md:px-20 md:py-6 md:mb-0">
@@ -145,124 +210,44 @@ const ReferalLanding = () => {
               <p>Nao Now Today.</p>
               <p>Opportunities Tomorrow.</p>
             </div>
-            <p className="flex items-center gap-2 hover:text-color-purple hover:cursor-pointer">
-              <IoMdMail className="text-xl" /> support@naonow.com
-            </p>
-
-            <p className="flex items-center gap-2 hover:text-color-purple hover:cursor-pointer">
-              <FaPhone className="text-xl" /> 02-3498-6970
-            </p>
-
-            <p className="flex items-center gap-2 hover:text-color-purple hover:cursor-pointer">
-              <FaMapMarkerAlt className="text-xl" /> 325 N. Saint Paul Street,
-              Suite 3100, Dallas, TX 75201
-            </p>
+            {contactInfo.map((elem) => (
+              <p
+                key={elem.text}
+                className="flex items-center gap-2 hover:text-color-purple hover:cursor-pointer"
+              >
+                {elem.icon} {elem.text}
+              </p>
+            ))}
           </div>
           <div>
             <div className="hidden md:block mb-4">
               <p className="font-bold text-lg mb-2">QUICK LINKS</p>
               <ul className="text-sm space-y-2">
-                <li className="hover:text-color-purple transition-colors">
-                  <a
-                    href="https://www.naonow.com/curriculum"
-                    target="_blank"
-                    rel="noreferrer"
+                {quickLinks.map((link) => (
+                  <li
+                    key={link.title}
+                    className="hover:text-color-purple transition-colors"
                   >
-                    Speaking Course
-                  </a>
-                </li>
-                <li className="hover:text-color-purple transition-colors">
-                  <a
-                    href="https://www.naonow.com/writing"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Writing Course
-                  </a>
-                </li>
-                <li className="hover:text-color-purple transition-colors">
-                  <a
-                    href="https://www.naonow.com/mentors"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Mentors
-                  </a>
-                </li>
-                <li className="hover:text-color-purple transition-colors">
-                  <a
-                    href="https://www.naonow.com/faq"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    FAQ
-                  </a>
-                </li>
-                <li className="hover:text-color-purple transition-colors">
-                  <a
-                    href="https://dashboard.naonow.com/onboarding"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Purchase
-                  </a>
-                </li>
-                <li className="hover:text-color-purple transition-colors">
-                  <a
-                    href="https://www.naonow.com/contact-us"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Contact Us
-                  </a>
-                </li>
+                    <a href={link.href} target="_blank" rel="noreferrer">
+                      {link.title}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
             <p className="font-semibold text-lg mb-2">Follow us</p>
             <div className="flex gap-4">
-              <a
-                href="https://www.instagram.com/naonowmentoring/"
-                target="_blank"
-                rel="noreferrer"
-                className="p-[10px] block bg-white bg-opacity-20 text-xl w-10 rounded-lg hover:bg-opacity-100 hover:text-color-dark-violet transition-colors"
-              >
-                <FaInstagram />
-              </a>
-
-              <a
-                href="https://pf.kakao.com/_XdGUb"
-                target="_blank"
-                rel="noreferrer"
-                className="p-[10px] block bg-white bg-opacity-20 text-xl w-10 rounded-lg hover:bg-opacity-100 hover:text-color-dark-violet transition-colors"
-              >
-                <RiKakaoTalkFill />
-              </a>
-
-              <a
-                href="https://www.facebook.com/naonowmentoring"
-                target="_blank"
-                rel="noreferrer"
-                className="p-[10px] block bg-white bg-opacity-20 text-xl w-10 rounded-lg hover:bg-opacity-100 hover:text-color-dark-violet transition-colors"
-              >
-                <IoLogoFacebook />
-              </a>
-              <a
-                href="https://www.linkedin.com/company/nao-now-mentoring"
-                target="_blank"
-                rel="noreferrer"
-                className="p-[10px] block bg-white bg-opacity-20 text-xl w-10 rounded-lg hover:bg-opacity-100 hover:text-color-dark-violet transition-colors"
-              >
-                <FaLinkedin />
-              </a>
-
-              <a
-                href="https://blog.naver.com/naonowmentoring/"
-                target="_blank"
-                rel="noreferrer"
-                className="p-[10px] block bg-white bg-opacity-20 text-xl w-10 rounded-lg hover:bg-opacity-100 hover:text-color-dark-violet transition-colors"
-              >
-                <SiNaver />
-              </a>
+              {socialLinks.map((link) => (
+                <a
+                  key={link.href}
+                  href={link.href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="p-[10px] block bg-white bg-opacity-20 text-xl w-10 rounded-lg hover:bg-opacity-100 hover:text-color-dark-violet transition-colors"
+                >
+                  {link.icon}
+                </a>
+              ))}
             </div>
           </div>
         </section>
