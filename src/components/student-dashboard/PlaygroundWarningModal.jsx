a06f04cd464b11238a2f83f7690f11be0e0dc5c7
igor <pythagoras1206@gmail.com>
2024-06-14 20:03:47 +0900

    fix: prettier format

--- src/components/student-dashboard/PlaygroundWarningModal.jsx
+++ src/components/student-dashboard/PlaygroundWarningModal.jsx
@@ -9,10 +9,14 @@ const PlaygroundWarningModal = ({ isWarningOpen, closeModal }) => {
       <div>
         <div className="flex items-center justify-between mb-5">
           <div>
-            <h2 className="text-2xl font-semibold">{t('playground_modal_title')}</h2>
+            <h2 className="text-2xl font-semibold">
+              {t('playground_modal_title')}
+            </h2>
           </div>
         </div>
-        <div className="w-full text-center text-lg">{t('playground_modal_desc')}</div>
+        <div className="w-full text-center text-lg">
+          {t('playground_modal_desc')}
+        </div>
       </div>
     </ModalWrapper>
   );
--- src/pages/Students/Subscriptions/SubscriptionCard.jsx
+++ src/pages/Students/Subscriptions/SubscriptionCard.jsx
@@ -1,62 +1,62 @@
-import { useTranslation } from "react-i18next";
-import { FaCheck } from "react-icons/fa6";
-import Indicator from "src/components/Indicator";
-import { currencyFormat } from "src/shared/utils/currencyFormat";
+import { useTranslation } from 'react-i18next';
+import { FaCheck } from 'react-icons/fa6';
+import Indicator from 'src/components/Indicator';
+import { currencyFormat } from 'src/shared/utils/currencyFormat';
 
 export const SubscriptionCard = ({
-	title,
-	sessionsPerWeek,
-	totalSessions,
-	price,
-	months,
-	duration,
-	credits,
-	active,
-	isReferral = false,
+  title,
+  sessionsPerWeek,
+  totalSessions,
+  price,
+  months,
+  duration,
+  credits,
+  active,
+  isReferral = false,
 }) => {
-	const [t] = useTranslation(["common", "lessons"]);
-	return (
-		<div
-			className={`w-full p-4 rounded-[10px] border border-solid select-none`}
-		>
-			<div className="text-lg font-bold capitalize mb-3">
-				{isReferral && "🎁 "}
-				{title}
-			</div>
+  const [t] = useTranslation(['common', 'lessons']);
+  return (
+    <div
+      className={`w-full p-4 rounded-[10px] border border-solid select-none`}
+    >
+      <div className="text-lg font-bold capitalize mb-3">
+        {isReferral && '🎁 '}
+        {title}
+      </div>
 
-			{!isReferral && (
-				<div className="text-sm font-normal mb-4">
-					{currencyFormat({ number: price })}
-				</div>
-			)}
-			{active && credits > 0 && (
-				<Indicator className="bg-[#02C97E] text-[#02C97E] mb-4 bg-opacity-10">
-					<FaCheck /> {t("active_subscription")}
-				</Indicator>
-			)}
-			<div className="flex">
-				<div className="flex flex-wrap items-center text-sm font-light leading-5">
-					<div className="text-[13px] text-gray-400">
-						{months} {t("months", { ns: "lessons" })}
-					</div>
-					<span className="w-1 h-1 bg-gray-300 rounded-full mx-[6px]" />
-					<div className="text-[13px] text-gray-400">
-						{sessionsPerWeek} {t("lesson_per_week").toLowerCase()}
-					</div>
-					<span className="w-1 h-1 bg-gray-300 rounded-full mx-[6px]" />
-					<div className="text-[13px] text-gray-400">
-						{duration} {t("minutes_full").toLowerCase()}
-					</div>
-					<span className="w-1 h-1 bg-gray-300 rounded-full mx-[6px]" />
-					<div className="text-[13px] text-gray-400">
-						{totalSessions} {t("lessons_total").toLowerCase()}
-					</div>
-					<span className="w-1 h-1 bg-gray-300 rounded-full mx-[6px]" />
-					<div className="text-[13px] text-gray-400">
-						{credits}/{totalSessions} {t("remaining_credits").toLowerCase()}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+      {!isReferral && (
+        <div className="text-sm font-normal mb-4">
+          {currencyFormat({ number: price })}
+        </div>
+      )}
+      {active && credits > 0 && (
+        <Indicator className="bg-[#02C97E] text-[#02C97E] mb-4 bg-opacity-10">
+          <FaCheck /> {t('active_subscription')}
+        </Indicator>
+      )}
+      <div className="flex">
+        <div className="flex flex-wrap items-center text-sm font-light leading-5">
+          <div className="text-[13px] text-gray-400">
+            {months} {t('months', { ns: 'lessons' })}
+          </div>
+          <span className="w-1 h-1 bg-gray-300 rounded-full mx-[6px]" />
+          <div className="text-[13px] text-gray-400">
+            {sessionsPerWeek} {t('lesson_per_week').toLowerCase()}
+          </div>
+          <span className="w-1 h-1 bg-gray-300 rounded-full mx-[6px]" />
+          <div className="text-[13px] text-gray-400">
+            {duration} {t('minutes_full').toLowerCase()}
+          </div>
+          <span className="w-1 h-1 bg-gray-300 rounded-full mx-[6px]" />
+          <div className="text-[13px] text-gray-400">
+            {totalSessions} {t('lessons_total').toLowerCase()}
+          </div>
+          <span className="w-1 h-1 bg-gray-300 rounded-full mx-[6px]" />
+          <div className="text-[13px] text-gray-400">
+            {credits}/{totalSessions} {t('remaining_credits').toLowerCase()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
