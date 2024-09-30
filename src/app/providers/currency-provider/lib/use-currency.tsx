de700eee21d375e20c40ada89e82ca7542453c00
igor <pythagoras1206@gmail.com>
2024-09-30 19:40:40 +0900

    fix: subscriptions build

--- src/app/providers/currency-provider/lib/use-currency.tsx
+++ src/app/providers/currency-provider/lib/use-currency.tsx
@@ -1,10 +1,10 @@
 import type { CurrencyDictionary } from '@/shared/constants/global';
-import { createContext, type Dispatch, type SetStateAction, useContext } from 'react';
+import { type Dispatch, type SetStateAction, createContext, useContext } from 'react';
 
 interface CurrencyContextType {
   loadingCurrency: boolean;
   curCurrency?: CurrencyDictionary;
-  findCurrency?: (value: CurrencyDictionary) => CurrencyDictionary | undefined;
+  findCurrency?: (value: string) => CurrencyDictionary | undefined;
   setCurCurrency?: Dispatch<SetStateAction<CurrencyDictionary | undefined>>;
   setLoadingCurrency?: Dispatch<SetStateAction<boolean>>;
 }
