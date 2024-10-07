57cb8c752f98cb70e9d6e212a379f0f7dcedf9bc
igor <pythagoras1206@gmail.com>
2024-10-07 20:55:33 +0900

    fix: currencies -> currency

--- src/components/buy-package/promo-modal.tsx
+++ src/components/buy-package/promo-modal.tsx
@@ -2,10 +2,10 @@ import Button from '@/components/form/button';
 import InputField from '@/components/form/input-field';
 import Loader from '@/components/loader/loader';
 import { APPLY_PROMOTION_CODE_FOR_PACKAGE_RESOLVER } from '@/shared/apollo/graphql';
-import { Currencies, DiscountType } from '@/shared/constants/global';
+import { DiscountType } from '@/shared/constants/global';
 import { currencyFormat } from '@/shared/utils/currency-format';
 import notify from '@/shared/utils/notify';
-import type { Mutation, Package } from '@/types/types.generated';
+import { Currency, type Mutation, type Package } from '@/types/types.generated';
 import { useMutation } from '@apollo/client';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
@@ -40,7 +40,7 @@ export const PromoModal = ({
       variables: {
         code: promo,
         packageId: selectedPackage.id,
-        currency: (localStorage.getItem('currency') || Currencies.KRW).toLowerCase(),
+        currency: (localStorage.getItem('currency') || Currency.Krw).toLowerCase(),
       },
       onCompleted: (data) => {
         if (!data.applyPromotionCodeForPackage.promotionCode) {
